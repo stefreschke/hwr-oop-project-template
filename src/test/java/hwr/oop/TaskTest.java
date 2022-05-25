4c6c51070afd9493fc8c8a6da9d2efbd549e42d4
Emma <emma-sophie.betz@web.de>
2022-05-25 16:38:29 +0200

    Tests for Task.java

---
+++ src/test/java/hwr/oop/TaskTest.java
@@ -0,0 +1,34 @@
+package hwr.oop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class TaskTest {
+    Task task;
+    @BeforeEach
+    void setUp() {
+        task = new Task("vacuum", "25.05.22", "todo", "27.05.22");
+    }
+
+    @Test
+    void task_getTaskName_returnsGivenTaskName() {
+        assertThat(task.getTaskName()).isEqualTo("vacuum");
+    }
+
+    @Test
+    void task_getDate_returnsGivenDate() {
+        assertThat(task.getDate()).isEqualTo("25.05.22");
+    }
+
+    @Test
+    void task_getKanbanLabel_returnsGivenLabel() {
+        assertThat(task.getKanbanLabel()).isEqualTo("todo");
+    }
+
+    @Test
+    void task_getDeadline_returnsGivenDeadline() {
+        assertThat(task.getDeadline()).isEqualTo("27.05.22");
+    }
+}
