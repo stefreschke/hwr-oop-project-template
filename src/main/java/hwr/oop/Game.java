4da5bcea0899838948114b67f409db45fdd25eb1
FabianR <FabianR@lucanet.com>
2022-06-07 14:22:43 +0200

    added Game class and some tests

--- src/main/java/hwr/oop/Game.java
+++ src/main/java/hwr/oop/Game.java
@@ -1,4 +1,12 @@
 package hwr.oop;
 
 public class Game {
+  public void roll(int pinsHit) throws Exception {
+    if (pinsHit < 0) {
+      throw new Exception("Cannot knock down less than 0 pins");
+    }
+    if (10 < pinsHit) {
+      throw new Exception("Cannot knock down more than 10 pins");
+    }
+  }
 }
--- src/test/java/hwr/oop/GameTest.java
+++ src/test/java/hwr/oop/GameTest.java
@@ -1,4 +1,33 @@
 package hwr.oop;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class GameTest {
+  Game game;
+
+  @BeforeEach
+  void instatiateGame() {
+    game = new Game();
+  }
+
+  @Test
+  void gameIsCreatedCorrectlyAndRollsCanBePerformed() {
+    assertThat(game).isNotEqualTo(null);
+
+    assertThatNoException().isThrownBy(() -> {
+      game.roll(0);
+      game.roll(1);
+      game.roll(10);
+    });
+  }
+
+  @Test
+  void cannotHitLessThanZeroOrMoreThanTenPins() {
+    assertThatExceptionOfType(Exception.class).isThrownBy(() -> game.roll(-1));
+    assertThatExceptionOfType(Exception.class).isThrownBy(() -> game.roll(11));
+  }
+
 }
