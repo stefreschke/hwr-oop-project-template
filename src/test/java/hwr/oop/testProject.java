f408d5ef366e2cc009e778d3d3d28f2203d5196a
ChristianSteiner <cs.christian.steiner@gmail.com>
2023-04-11 19:13:43 +0200

    changed Code Testcase

--- src/test/java/hwr/oop/testProject.java
+++ src/test/java/hwr/oop/testProject.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import project.creation.Project;
 import project.creation.Task;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -16,13 +17,19 @@ public class testProject {
         Task t = new Task();
         case1.addTask(t);
         Assertions.assertEquals(case1,case1);
+        System.out.println("project without error created");
     }
 
     @Test
     void project_changeDeadline(){
         Task t = new Task();
         case2.addTask(t);
+        System.out.println(case1.getDate());
+        System.out.println(case2.getDate());
         case2.changeDeadline(LocalDate.now());
+        System.out.println(case1.getDate());
+        System.out.println(case2.getDate());
         Assertions.assertEquals(case1.getDate(),case2.getDate());
+        System.out.println("changeDeadline() ran without error");
     }
 }
