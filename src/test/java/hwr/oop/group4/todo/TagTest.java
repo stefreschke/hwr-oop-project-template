4324adef274a839cd379e2ade83fcb71ad2d7332
Marvin Waldmann <marvin.waldmann@sap.com>
2023-04-11 10:27:10 +0200

    Tag test class renamed

--- src/test/java/hwr/oop/group4/todo/TagTest.java
+++ src/test/java/hwr/oop/group4/todo/TagTest.java
@@ -1,12 +1,10 @@
 package hwr.oop.group4.todo;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-class ProjectTest {
+class TagTest {
 
     @Test
     void createTag() {
