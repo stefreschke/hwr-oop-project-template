d6fe496b6dcd430ab0ae10fae956d55324894349
MuffinCPT <benjamin.liebenberg04@gmail.com>
2023-04-11 17:18:57 +0200

    removed Classes

--- src/main/java/project/Project.java
+++
@@ -1,4 +0,0 @@
-package project;
-
-public class Project {
-}
--- src/test/java/testobjects/CanCreateNewProject.java
+++
@@ -1,8 +0,0 @@
-package testobjects;
-
-import project.Project;
-
-public class CanCreateNewProject {
-    public Project.project project = new Project();
-    ProjectID projects = procect.ID();
-}
