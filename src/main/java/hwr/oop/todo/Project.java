cf265dac50cf41f401dccccd94cadae51a25c550
Jason Piyasron <jason.piyasron@gmx.net>
2023-04-11 13:32:19 +0200

    Added testing methods. Disabled project attributes.

--- src/main/java/hwr/oop/todo/Project.java
+++ src/main/java/hwr/oop/todo/Project.java
@@ -2,9 +2,11 @@ package hwr.oop.todo;
 
 class Project {
     String title;
+ /*
     Task task;
     Status status;
     Date deadline;
+ */
 
 
 }
--- src/test/java/hwr/oop/todo/ProjectTest.java
+++ src/test/java/hwr/oop/todo/ProjectTest.java
@@ -1,12 +1,31 @@
 package hwr.oop.todo;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class ProjectTest {
     @Test
-    void createProjectTest() {
+    void createProject() {
         Project project = new Project();
         Assertions.assertNotEquals(project,null);
+    }
+
+    @Test
+    @Disabled
+    void addTask(){
+        Project project = new Project();
+    }
+
+    @Test
+    @Disabled
+    void determineStatus(){
+        Project project = new Project();
+    }
+
+    @Test
+    @Disabled
+    void setDeadline(){
+
     }
 }
