7827380a5a097f4abb5d094efe3de056d46d07c8
Jason Piyasron <jason.piyasron@gmx.net>
2023-04-18 09:07:42 +0200

    Corrected Test with LocalDate

--- src/test/java/hwr/oop/todo/ProjectTest.java
+++ src/test/java/hwr/oop/todo/ProjectTest.java
@@ -11,13 +11,13 @@ import static org.assertj.core.api.Assertions.*;
 class ProjectTest {
     @Test
     void testProject() {
-        Assertions.assertDoesNotThrow(() -> new Project("Test",LocalDate.of(2000,1,1)));
+        Assertions.assertDoesNotThrow(() -> new Project("Test", LocalDate.of(2000, 1, 1)));
     }
 
     @Test
     @Disabled
-    void addTask(){
-        Project project = new Project("",LocalDate.of(2000,1,1));
+    void addTask() {
+        Project project = new Project("", LocalDate.of(2000, 1, 1));
         /*
         Task task = new Task("");
         project.addTask(task);
@@ -26,9 +26,9 @@ class ProjectTest {
     }
 
     @Test
-    void testDeadline(){
-        Project project = new Project("",null);
-        assertThat(project.getDeadline()).isEqualTo("00.00.0000");
+    void testDeadline() {
+        Project project = new Project("", LocalDate.of(2000, 1, 1));
+        assertThat(project.getDeadline()).isEqualTo(LocalDate.of(2000, 1, 1));
         LocalDate date = LocalDate.of(2024, 4, 4);
         project.setDeadline(date);
         assertThat(date).isEqualTo(project.getDeadline());
