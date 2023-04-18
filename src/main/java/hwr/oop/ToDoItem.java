b27d4f90fa2fad50a83c0031324e3750589a8af8
Sander Hahn <sander.hahn@helmholtz-berlin.de>
2023-04-18 13:35:20 +0200

    setTag & getLocalDate for creation date of task

--- src/main/java/hwr/oop/ToDoItem.java
+++ src/main/java/hwr/oop/ToDoItem.java
@@ -1,36 +1,58 @@
 package hwr.oop;
 
-public class ToDoItem {
+import java.time.LocalDate;
 
+public class ToDoItem {
+    private final int id;
     private String title;
     private String description;
+    private String tag;
     private boolean done;
     private Priority priority;
 
-    public ToDoItem () {
-        this.title = "New Item";
-        this.description = "";
-        this.done = false;
-        this.priority = Priority.LOW;
+    public ToDoItem (int id, String title, String description, String tag, boolean done, Priority priority) {
+        this.id = id;
+        this.title = title;
+        this.description = description + "\nCreated " + getLocalDate();
+        this.tag = tag;
+        this.done = done;
+        this.priority = priority;
     }
-
-    // setter
     void setTitle(String title) {
         this.title = title;
     }
-
     void setDescription(String description) {
-        this.description = description;
+        this.description = description + "\nCreated " + getLocalDate();
     }
-
     void setDone(boolean done) {
         this.done = done;
     }
-
     void setPriority(Priority priority) {
         this.priority = priority;
     }
+    void setTag(String tag) {
+        this.tag = tag;
+    }
 
-
-    // getter
+    static String getLocalDate() {
+        return LocalDate.now().toString();
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getTag() {
+        return tag;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public boolean isDone() {
+        return done;
+    }
+    public Priority getPriority() {
+        return priority;
+    }
+    public int getId() {
+        return id;
+    }
 }
--- src/test/java/hwr/oop/ListJsonTest.java
+++ src/test/java/hwr/oop/ListJsonTest.java
@@ -2,15 +2,37 @@ package hwr.oop;
 
 import org.junit.jupiter.api.Test;
 
-import javax.lang.model.element.Name;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ListJsonTest {
     @Test
-    public void listJsonTest() {
+    public void listToJsonTest() {
         List assertList = new List();
-        assertList.setName("assertName");
-        assertList.writeToJSON();
-        List list = new List();
+        assertList.setName("TestName");
+        assertList.writeToJSON("listTest.mp3");
+        try {
+            StringBuilder jsonIn;
+            try (FileReader reader = new FileReader("listTest.json")) {
+                int character;
+                jsonIn = new StringBuilder();
+                while ((character = reader.read()) != -1) {
+                    jsonIn.append((char) character);
+                }
+            }
+            assertThat(jsonIn.toString()).isEqualTo("{\"Name\":\"TestName\"}");
+        } catch (IOException e) {
+            System.out.println("File not found");
+        } catch (NullPointerException e) {
+            System.out.println("File is empty");
+        }
+    }
+    @Test
+    public void jsonToListTest() {
+        Program program = new Program();
+        List assertList = program.loadList("listTest.json");
+        assertThat(assertList.Name).isEqualTo("TestName");
     }
 }
+
