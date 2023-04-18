c46f18167d9144f7ee5273aa1019c5574b633d44
Caroline_Schaefer <caroline.schaefer@sap.com>
2023-04-18 12:52:52 +0200

    project-test split

--- src/test/java/hwr/oop/group4/todo/ProjectTest.java
+++ src/test/java/hwr/oop/group4/todo/ProjectTest.java
@@ -1,26 +1,54 @@
 package hwr.oop.group4.todo;
 
-import hwr.oop.group4.todo.builder.TaskBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ProjectTest {
 
+    private Project createProject(){
+        var beginAt = LocalDateTime.now();
+        var endAt = LocalDateTime.now();
+
+        return new Project("myProject", "myDesc", new HashSet<>(), new HashSet<>(), beginAt , endAt );
+    }
     @Test
-    void createProject() {
+    void canCreateProject() {
+        Project project = createProject();
+
+        assertThat(project.getName()).isEqualTo("myProject");
+    }
+
+    @Test
+    void canGetDescription(){
+        Project project = createProject();
+
+        assertThat(project.getDescription()).isEqualTo("myDesc");
+    }
+
+    @Test
+    void canGetTag(){
+        Project project = createProject();
+
+        assertThat(project.getTags().size()).isEqualTo(0);
+    }
+
+    @Test
+    void canGetTasks(){
+        Project project = createProject();
+
+        assertThat(project.getTasks().size()).isEqualTo(0);
+    }
+
+    @Test
+    void  canGetTime(){
         var beginAt = LocalDateTime.now();
         var endAt = LocalDateTime.now();
         Project project = new Project("myProject", "myDesc", new HashSet<>(), new HashSet<>(), beginAt, endAt);
 
-        assertThat(project.getName()).isEqualTo("myProject");
-        assertThat(project.getDescription()).isEqualTo("myDesc");
-        assertThat(project.getTags().size()).isEqualTo(0);
-        assertThat(project.getTasks().size()).isEqualTo(0);
         assertThat(project.getBegin()).isEqualTo(beginAt);
         assertThat(project.getEnd()).isEqualTo(endAt);
     }
