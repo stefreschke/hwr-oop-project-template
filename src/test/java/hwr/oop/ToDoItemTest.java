8b7e54a0f636f89ad01cb0506e2905aab25622eb
Sander Hahn <sander.hahn@helmholtz-berlin.de>
2023-04-18 13:35:20 +0200

    setTag & getLocalDate for creation date of task

--- src/test/java/hwr/oop/ToDoItemTest.java
+++ src/test/java/hwr/oop/ToDoItemTest.java
@@ -1,39 +1,76 @@
 package hwr.oop;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
 
-public class ToDoItemTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ToDoItemTest {
     @Test
-    public void itemTitleSetterStringTest() {
-        ToDoItem item = new ToDoItem();
+    void getIdTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
+        int result = item.getId();
+        assertThat(result).isEqualTo(0);
+    }
+    @Test
+    void getTitleTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
+        String result = item.getTitle();
+        assertThat(result).isEqualTo("testTitle");
+    }
+
+    @Test
+    void getDescriptionTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
+        String result = item.getDescription();
+        assertThat(result).isEqualTo("testDesc"+ "\nCreated " + ToDoItem.getLocalDate());
+    }
+
+    @Test
+    void getTagTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
+        String result = item.getTag();
+        assertThat(result).isEqualTo("testTag");
+    }
+
+    @Test
+    void isDoneTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
+        boolean result = item.isDone();
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    void getPriorityTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
+        Priority result = item.getPriority();
+        assertThat(result).isEqualTo(Priority.LOW);
+    }
+    @Test
+    void setTitleTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
         item.setTitle("Title");
         String testTitle = item.getTitle();
         assertThat(testTitle).isEqualTo("Title");
     }
     @Test
-    public void itemDescriptionSetterTest() {
-        ToDoItem item = new ToDoItem();
+    void setDescriptionTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
         item.setDescription("Description");
         String testDescription = item.getDescription();
         assertThat(testDescription).isEqualTo("Description");
     }
     @Test
-    public void itemDoneSetterTest() {
-        ToDoItem item = new ToDoItem();
+    void setDoneTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
         item.setDone(true);
         boolean testDone = item.isDone();
         assertThat(testDone).isEqualTo(true);
     }
-   @Test
-    public void itemPrioritySetterTest() {
-        ToDoItem item = new ToDoItem();
+    @Test
+    void setPriorityTest() {
+        ToDoItem item = new ToDoItem(0,"testTitle", "testDesc", "testTag", false, Priority.LOW);
         item.setPriority(Priority.HIGH);
         Priority testPriority = item.getPriority();
         assertThat(testPriority).isEqualTo(Priority.HIGH);
     }
-
 }
-
-
-
