bd390d239e49fd34474b1aa7d783f8d16f8a9c09
Sander Hahn <sander.hahn@helmholtz-berlin.de>
2023-04-19 13:00:58 +0200

    setter in den constructor & test klassen erstellt

--- src/main/java/hwr/oop/Project.java
+++ src/main/java/hwr/oop/Project.java
@@ -1,9 +1,15 @@
 package hwr.oop;
 
+import java.util.List;
+
 public class Project {
     public String title;
+    public List<ToDoItem> toDoList;
 
     public void setTitle(String title) {
         this.title = title;
+    }
+    public void setList(List<ToDoItem> toDoList) {
+        this.toDoList = toDoList;
     }
 }
