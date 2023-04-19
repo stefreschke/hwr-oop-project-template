55862b8fb4029f73769cd9b5afad1001cc5e4aab
vreoio <s_bartelsp22@stud.hwr-berlin.de>
2023-04-19 19:13:07 +0200

    Add DialogHelper to ProjectUi

--- src/main/java/hwr/oop/group4/todo/ui/ProjectUi.java
+++ src/main/java/hwr/oop/group4/todo/ui/ProjectUi.java
@@ -7,20 +7,19 @@ import hwr.oop.group4.todo.TodoList;
 import java.io.PrintStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ProjectUi {
 
     private final PrintStream out;
     private final Scanner in;
+    private final DialogHelper dialogHelper;
     private TodoList todoList;
 
     public ProjectUi(PrintStream out, Scanner in) {
         this.out = out;
         this.in = in;
+        dialogHelper = new DialogHelper(out, in);
     }
 
     public void menu(TodoList todoList) {
@@ -29,19 +28,19 @@ public class ProjectUi {
     }
 
     private void listProjects() {
-        String format = "%2d | %15.15s | %30.30s | %10.10s | %6.6s | %6.6s";
+        String format = "%2d | %15.15s | %30.30s | %10.10s | %6.6s | %6.6s%n";
         final List<Project> projects = todoList.getProjects();
 
-        final int idColumnLength = Math.max((int) Math.ceil(Math.log10(projects.size()) -2), 0);
+        final int idColumnLength = Math.max((int) Math.ceil(Math.log10(projects.size()) - 2), 0);
 
         out.println("ID" + (" ").repeat(idColumnLength) + " | Name            | Description                    | Tags       | Begin  | End   ");
         out.println("==" + ("=").repeat(idColumnLength) + "==================================================================================");
         for (int i = 0; i < projects.size(); i++) {
             final Project project = projects.get(i);
-            out.println(String.format(format,i, project.getName(), project.getDescription(),
+            out.printf(format, i, project.getName(), project.getDescription(),
                     concatTagsToString(project.getTags()),
                     project.getBegin().format(DateTimeFormatter.ofPattern("dd.MM.")),
-                    project.getEnd().format(DateTimeFormatter.ofPattern("dd.MM."))));
+                    project.getEnd().format(DateTimeFormatter.ofPattern("dd.MM.")));
         }
     }
 
