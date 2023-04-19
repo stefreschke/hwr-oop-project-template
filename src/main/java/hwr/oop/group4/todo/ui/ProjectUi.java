af0def3ce392165ac897881721632e6c4a3b496b
vreoio <s_bartelsp22@stud.hwr-berlin.de>
2023-04-19 20:19:18 +0200

    Add editProject method as placeholder

--- src/main/java/hwr/oop/group4/todo/ui/ProjectUi.java
+++ src/main/java/hwr/oop/group4/todo/ui/ProjectUi.java
@@ -49,6 +49,12 @@ public class ProjectUi {
                     // TODO: call upcoming TaskUi
                     break;
                 case "edit":
+                    id = dialogHelper.getValidIdFromUser("Enter the ID of the project you wish to edit: ", size);
+                    if (id == null) {
+                        out.println("There are no projects to edit.");
+                    } else {
+                        editProject(id);
+                    }
                     break;
                 case "remove":
                     id = dialogHelper.getValidIdFromUser("Enter the ID of the project you wish to remove: ", size);
@@ -105,6 +111,12 @@ public class ProjectUi {
                 .build();
 
         todoList.addProject(project);
+        int newId = todoList.getProjects().indexOf(project);
+
+        String question = "Do you want to open the edit menu to further customize the new project?";
+        if (dialogHelper.getYesNoFromUser(question, false)) {
+            editProject(newId);
+        }
     }
 
     private void removeProject(int id) {
@@ -112,6 +124,46 @@ public class ProjectUi {
         String confirmation = "Do you really want to remove " + projectName + "?";
         if (dialogHelper.getYesNoFromUser(confirmation, false)) {
             // TODO: delete a Project
+        }
+    }
+
+    private void editProject(int id) {
+        Project project = todoList.getProjects().get(id);
+
+        out.println("Name:        " + project.getName());
+        out.println("Description: " + project.getDescription());
+        out.println("Tags:        " + concatTagsToString(project.getTags()));
+        out.println("Begin:       " + project.getBegin().format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm")));
+        out.println("End:         " + project.getEnd().format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm")));
+
+        Map<String, String> options = new LinkedHashMap<>();
+        options.put("name",  "");
+        options.put("desc",  "");
+        options.put("tags",  "");
+        options.put("begin", "");
+        options.put("end",   "");
+        options.put("quit",  "Quit to the previous menu.");
+
+        while (true) {
+            String input = dialogHelper.getMenuSelectionFromUser("Edit a project.", "projects> edit> ", options);
+
+            // TODO: edit the project
+            switch (input) {
+                case "name":
+                    break;
+                case "desc":
+                    break;
+                case "tags":
+                    break;
+                case "begin":
+                    break;
+                case "end":
+                    break;
+                case "quit":
+                    return;
+                default:
+                    break;
+            }
         }
     }
 
