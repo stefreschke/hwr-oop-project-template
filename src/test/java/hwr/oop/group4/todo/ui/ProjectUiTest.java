de525429645c81b22681dc81fd571eb321315741
vreoio <s_bartelsp22@stud.hwr-berlin.de>
2023-04-21 16:22:48 +0200

    Add tests for ProjectUi: canListProjects()

--- src/test/java/hwr/oop/group4/todo/ui/ProjectUiTest.java
+++ src/test/java/hwr/oop/group4/todo/ui/ProjectUiTest.java
@@ -1,10 +1,14 @@
 package hwr.oop.group4.todo.ui;
 
+import hwr.oop.group4.todo.Project;
+import hwr.oop.group4.todo.TodoList;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ProjectUiTest {
 
@@ -17,8 +21,60 @@ public class ProjectUiTest {
         return new ByteArrayInputStream(inputInBytes);
     }
 
+    private TodoList getExampleTodoList(boolean addExampleProjects) {
+        TodoList todoList = new TodoList();
+
+        if (addExampleProjects) {
+            todoList.addProject(new Project.ProjectBuilder()
+                    .name("TEst")
+                    .description("Desc")
+                    .begin(LocalDateTime.of(2013, 12, 12, 15, 45))
+                    .end(LocalDateTime.of(2014, 12, 12, 12, 12))
+                    .build());
+            todoList.addProject(new Project.ProjectBuilder()
+                    .name("proj")
+                    .description("qwer")
+                    .begin(LocalDateTime.of(2003, 12, 22, 5, 45))
+                    .end(LocalDateTime.of(2014, 1, 10, 12, 12))
+                    .build());
+        }
+
+        return todoList;
+    }
+
     @Test
     void canListProjects() {
-        // TODO
+        Scanner inputStream = new Scanner(createInputStreamForInput("list\nquit\n"));
+        OutputStream outputStream = new ByteArrayOutputStream();
+
+        ProjectUi ui = new ProjectUi(new PrintStream(outputStream), inputStream);
+        ui.menu(getExampleTodoList(true));
+
+        String output = retrieveResultFrom(outputStream);
+
+        assertThat(output).isEqualTo(
+                "ID | Name            | Description                    | Tags       | Begin  | End   \n" +
+                "====================================================================================\n" +
+                " 0 |            TEst |                           Desc |            | 12.12. | 12.12.\n" +
+                " 1 |            proj |                           qwer |            | 22.12. | 10.01.\n" +
+                "Projects Menu\n" +
+                "                          list - List all projects.\n" +
+                "                           new - Add a new project.\n" +
+                "                         tasks - Open the task menu for a project.\n" +
+                "                          edit - Edit the attributes of a project.\n" +
+                "                        remove - Remove a project.\n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> ID | Name            | Description                    | Tags       | Begin  | End   \n" +
+                "====================================================================================\n" +
+                " 0 |            TEst |                           Desc |            | 12.12. | 12.12.\n" +
+                " 1 |            proj |                           qwer |            | 22.12. | 10.01.\n" +
+                "Projects Menu\n" +
+                "                          list - List all projects.\n" +
+                "                           new - Add a new project.\n" +
+                "                         tasks - Open the task menu for a project.\n" +
+                "                          edit - Edit the attributes of a project.\n" +
+                "                        remove - Remove a project.\n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> ");
     }
 }
