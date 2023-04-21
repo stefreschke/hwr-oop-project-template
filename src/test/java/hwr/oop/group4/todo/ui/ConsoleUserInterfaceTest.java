3e6522e4c6404d9ad724b6e428d907943169ac89
vreoio <s_bartelsp22@stud.hwr-berlin.de>
2023-04-21 16:51:44 +0200

    Add tests for ConsoleUserInterface: canOpenProjectsMenu()

--- src/test/java/hwr/oop/group4/todo/ui/ConsoleUserInterfaceTest.java
+++ src/test/java/hwr/oop/group4/todo/ui/ConsoleUserInterfaceTest.java
@@ -92,4 +92,44 @@ class ConsoleUserInterfaceTest {
                         "main> ");
     }
 
+    @Test
+    void canOpenProjectsMenu() {
+        InputStream inputStream = createInputStreamForInput("\nprotsch\nprojects\nquit\nquit\n");
+        OutputStream outputStream = new ByteArrayOutputStream();
+
+        ConsoleUserInterface ui = new ConsoleUserInterface(outputStream, inputStream);
+        ui.mainMenu();
+
+        String output = retrieveResultFrom(outputStream);
+
+        assertThat(output).isEqualTo(
+        "Do you want to load from a file? (Otherwise create an empty todo list)\n" +
+                "Answer y/Y/yes or n/N/no (leave empty for: no): Main Menu\n" +
+                "                        intray - \n" +
+                "                         tasks - \n" +
+                "                      projects - \n" +
+                "                      calendar - \n" +
+                "                          load - \n" +
+                "                          save - \n" +
+                "                          quit - Quit the program.\n" +
+                "main> main> ID | Name            | Description                    | Tags       | Begin  | End   \n" +
+                "====================================================================================\n" +
+                "Projects Menu\n" +
+                "                          list - List all projects.\n" +
+                "                           new - Add a new project.\n" +
+                "                         tasks - Open the task menu for a project.\n" +
+                "                          edit - Edit the attributes of a project.\n" +
+                "                        remove - Remove a project.\n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> Main Menu\n" +
+                "                        intray - \n" +
+                "                         tasks - \n" +
+                "                      projects - \n" +
+                "                      calendar - \n" +
+                "                          load - \n" +
+                "                          save - \n" +
+                "                          quit - Quit the program.\n" +
+                "main> ");
+    }
+
 }
