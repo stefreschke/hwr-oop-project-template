85a4444e499b591ae75c35d4ff9a38c2e0cfdae1
vreoio <s_bartelsp22@stud.hwr-berlin.de>
2023-04-21 16:33:47 +0200

    Add test for ProjectUi: canCreateNewProjectAndEdit()

--- src/test/java/hwr/oop/group4/todo/ui/ProjectUiTest.java
+++ src/test/java/hwr/oop/group4/todo/ui/ProjectUiTest.java
@@ -126,4 +126,64 @@ public class ProjectUiTest {
                     "projects> ");
     }
 
+    @Test
+    void canCreateNewProjectAndEdit() {
+        Scanner inputStream = new Scanner(createInputStreamForInput("new\nPeter\nParker\n12.12.2023\n24.12.2023\nyes\nquit\nlist\nquit\n"));
+        OutputStream outputStream = new ByteArrayOutputStream();
+
+        ProjectUi ui = new ProjectUi(new PrintStream(outputStream), inputStream);
+        ui.menu(getExampleTodoList(true));
+
+        String output = retrieveResultFrom(outputStream);
+
+        assertThat(output).isEqualTo(
+        "ID | Name            | Description                    | Tags       | Begin  | End   \n" +
+                "====================================================================================\n" +
+                " 0 |            TEst |                           Desc |            | 12.12. | 12.12.\n" +
+                " 1 |            proj |                           qwer |            | 22.12. | 10.01.\n" +
+                "Projects Menu\n" +
+                "                          list - List all projects.\n" +
+                "                           new - Add a new project.\n" +
+                "                         tasks - Open the task menu for a project.\n" +
+                "                          edit - Edit the attributes of a project.\n" +
+                "                        remove - Remove a project.\n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> Enter a name:        Enter a description: When should the project begin?\n" +
+                "The current date/time will be used if you leave this empty.\n" +
+                "Enter a date/time formatted as 'dd.mm.yyyy' or 'dd.mm.yyyy hh:mm': When should the project end?\n" +
+                "Enter a date/time formatted as 'dd.mm.yyyy' or 'dd.mm.yyyy hh:mm': Do you want to open the edit menu to further customize the new project?\n" +
+                "Answer y/Y/yes or n/N/no (leave empty for: no): Name:        Peter\n" +
+                "Description: Parker\n" +
+                "Tags:        \n" +
+                "Begin:       12.12.2023 - 00:00\n" +
+                "End:         24.12.2023 - 00:00\n" +
+                "Edit a project.\n" +
+                "                          name - \n" +
+                "                          desc - \n" +
+                "                          tags - \n" +
+                "                         begin - \n" +
+                "                           end - \n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> edit> Projects Menu\n" +
+                "                          list - List all projects.\n" +
+                "                           new - Add a new project.\n" +
+                "                         tasks - Open the task menu for a project.\n" +
+                "                          edit - Edit the attributes of a project.\n" +
+                "                        remove - Remove a project.\n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> ID | Name            | Description                    | Tags       | Begin  | End   \n" +
+                "====================================================================================\n" +
+                " 0 |            TEst |                           Desc |            | 12.12. | 12.12.\n" +
+                " 1 |            proj |                           qwer |            | 22.12. | 10.01.\n" +
+                " 2 |           Peter |                         Parker |            | 12.12. | 24.12.\n" +
+                "Projects Menu\n" +
+                "                          list - List all projects.\n" +
+                "                           new - Add a new project.\n" +
+                "                         tasks - Open the task menu for a project.\n" +
+                "                          edit - Edit the attributes of a project.\n" +
+                "                        remove - Remove a project.\n" +
+                "                          quit - Quit to the previous menu.\n" +
+                "projects> ");
+    }
+
 }
