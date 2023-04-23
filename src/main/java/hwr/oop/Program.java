9d29034e0a2e16556c1d434cba7f52daea44e540
Lukas Klockenhoff <lukasklockenhoff@icloud.com>
2023-04-23 16:35:42 +0200

    added ListClass and Programm Json Methods

--- src/main/java/hwr/oop/Program.java
+++ src/main/java/hwr/oop/Program.java
@@ -1,21 +1,20 @@
 package hwr.oop;
 
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 public class Program {
     public List loadList(String fileName) {
         Gson gson = new Gson();
-        List list = new List();
-        try {
-            JsonReader reader = new JsonReader(new FileReader(fileName));
-            list = gson.fromJson(reader, List.class);
-        } catch (FileNotFoundException e) {
+        String json = null;
+        try (FileReader reader = new FileReader(fileName + ".json")) {
+            char[] buffer = new char[1024];
+            int len = reader.read(buffer);
+            json = new String(buffer, 0, len);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return list;
+
+        return gson.fromJson(json, List.class);
     }
 }
