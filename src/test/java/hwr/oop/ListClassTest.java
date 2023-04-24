700f475370d45a1ec929b83a1d36c98561014bc9
Tobias Reetz <tobias.reetz@helmholtz-berlin.de>
2023-04-24 08:06:40 +0200

    Add createToDo und Test weitmöglichst erstellt

--- src/test/java/hwr/oop/ListClassTest.java
+++ src/test/java/hwr/oop/ListClassTest.java
@@ -12,8 +12,44 @@ public class ListClassTest {
     public void setListName(String Name) {
         List list = new List();
         list.setName(Name);
-        String TestName = list.Name;
+        String TestName = list.getName();
         assertThat(TestName).isEqualTo(Name);
     }
+/*
+    @ParameterizedTest
+    @ValueSource(strings = {"List", "New List"})
+    public void createToDodescriptionTest(String description) {
+        ToDoItem ToDo = new ToDoItem();
+        ToDo.setDescription(description);
+        String ToDodescription = ToDo.getdescription();
+        assertThat(ToDodescription).isEqualTo(description);
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"List", "New List"})
+    public void createToDotitleTest(String title) {
+        ToDoItem ToDo = new ToDoItem();
+        ToDo.setTitle(title);
+        String ToDotile = ToDo.gettitle();
+        assertThat(ToDotile).isEqualTo(title);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"List", "New List"})
+    public void createToDodoneTest(boolean done) {
+        ToDoItem ToDo = new ToDoItem();
+        ToDo.setDone(done);
+        String ToDodone = ToDo.getdone();
+        assertThat(ToDodone).isEqualTo(done);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"List", "New List"})
+    public void createToDodescriptionTest(Priority priority) {
+        ToDoItem ToDo = new ToDoItem();
+        ToDo.setPriority(priority);
+        String ToDopriority = ToDo.getpriority();
+        assertThat(ToDopriority).isEqualTo(priority);
+    }
+*/
 }
