3011619699ec979a35547b001c6601389fe1c979
Johannes <Johannes.Foltin@gmx.de>
2023-04-24 11:44:46 +0200

    Added createTask and getID method Added list for all Tasks

--- src/main/java/hwr/oop/clicode/Main.java
+++ src/main/java/hwr/oop/clicode/Main.java
@@ -1,7 +1,26 @@
 package hwr.oop.clicode;
 
+import hwr.oop.dataclasses.Task;
+import hwr.oop.dataclasses.TaskList;
+import hwr.oop.dataclasses.TaskState;
+import hwr.oop.dataclasses.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public List<Task> taskList;
+
     public static void main(String[] args) {
         System.out.println("we have a main class");
+    }
+    public void createTask(String title, String content, User creator,LocalDate deadline){
+        Task task = new Task(getID(),title,content,TaskState.IN_PROGRESS,null,creator, LocalDate.now(),deadline);
+        taskList.add(task);
+    }
+
+    public int getID(){
+        return 0;
     }
 }
--- src/main/java/hwr/oop/dataclasses/TaskList.java
+++ src/main/java/hwr/oop/dataclasses/TaskList.java
@@ -3,7 +3,7 @@ package hwr.oop.dataclasses;
 import java.util.List;
 
 public class TaskList {
-    private List<Task> taskList;
+    public List<Task> taskList;
 
     void sortByUser(){};
     //TODO Sortmethods
