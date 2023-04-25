73431d816affd7cb4448f0cf7a037e98ec290014
Marvin Waldmann <marvin.waldmann@sap.com>
2023-04-25 13:15:03 +0200

    formatting

--- src/test/java/hwr/oop/group4/todo/ui/controller/tables/TableTest.java
+++ src/test/java/hwr/oop/group4/todo/ui/controller/tables/TableTest.java
@@ -10,7 +10,12 @@ class TableTest {
 
     @Test
     void tableHead() {
-        final Table table = new Table(List.of(new ColumnConfig("first", 10), new ColumnConfig("2", 2)));
+        final Table table = new Table(
+                List.of(
+                        new ColumnConfig("first", 10),
+                        new ColumnConfig("2", 2)
+                )
+        );
         assertThat(table).hasToString(
                 "| first      | 2  |" + System.lineSeparator() +
                         "===================" + System.lineSeparator());
@@ -18,7 +23,12 @@ class TableTest {
 
     @Test
     void tableWithRows() {
-        final Table table = new Table(List.of(new ColumnConfig("id", 2), new ColumnConfig("cutOF", 2)));
+        final Table table = new Table(
+                List.of(
+                        new ColumnConfig("id", 2),
+                        new ColumnConfig("cutOF", 2)
+                )
+        );
         table.addRow("1", "this text is cut of");
         table.addRow("2", "cut of text");
         table.addRow("3", "text");
@@ -26,10 +36,11 @@ class TableTest {
 
         assertThat(table).hasToString(
                 "| id | cu |" + System.lineSeparator() +
-                        "===========" + System.lineSeparator() +
-                        "|  1 | th |" + System.lineSeparator() +
-                        "|  2 | cu |" + System.lineSeparator() +
-                        "|  3 | te |" + System.lineSeparator() +
-                        "|    |    |" + System.lineSeparator());
+                "===========" + System.lineSeparator() +
+                "|  1 | th |" + System.lineSeparator() +
+                "|  2 | cu |" + System.lineSeparator() +
+                "|  3 | te |" + System.lineSeparator() +
+                "|    |    |" + System.lineSeparator()
+        );
     }
 }
