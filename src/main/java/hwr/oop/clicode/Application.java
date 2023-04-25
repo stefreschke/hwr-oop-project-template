a6562f0cdfb8f8f86f138e6594e9d7b97ded2aa1
Johannes <Johannes.Foltin@gmx.de>
2023-04-25 08:49:38 +0200

    add taskList removed TaskList.java

--- src/main/java/hwr/oop/clicode/Application.java
+++ src/main/java/hwr/oop/clicode/Application.java
@@ -1,7 +1,6 @@
 package hwr.oop.clicode;
 
 import hwr.oop.dataclasses.Task;
-import hwr.oop.dataclasses.TaskList;
 import hwr.oop.dataclasses.TaskState;
 import hwr.oop.dataclasses.User;
 
@@ -9,8 +8,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-    public List<Task> taskList;
+public class Application {
+    private List<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("we have a main class");
