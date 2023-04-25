1281bffad82d823e5264b7046f90d7b73bde4bcf
MikaMk4 <mikailamelek@gmail.com>
2023-04-25 08:33:07 +0200

    Implemented getDate in Card class.

--- src/main/java/hwr/oop/cards/Card.java
+++ src/main/java/hwr/oop/cards/Card.java
@@ -5,18 +5,20 @@ import java.time.LocalDate;
 public class Card {
     private final String question;
     private final String answer;
+    private final LocalDate date;
 
     public Card(String question, String answer){
 
         this.question = question;
         this.answer = answer;
+        this.date = LocalDate.now();
     }
     public String getQuestion() {
         return question;
     }
 
     public LocalDate getDate() {
-        return null;
+        return date;
     }
 
     public String getAnswer() {
