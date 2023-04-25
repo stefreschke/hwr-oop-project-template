60864c8bd72c3d01f87cd4f594f0956e5924d4cd
Marvin Waldmann <marvin.waldmann@sap.com>
2023-04-25 13:45:06 +0200

    use Record for Tag

--- src/main/java/hwr/oop/group4/todo/Tag.java
+++ src/main/java/hwr/oop/group4/todo/Tag.java
@@ -1,29 +1,5 @@
 package hwr.oop.group4.todo;
 
-import java.util.Objects;
+public record Tag(String name) {
 
-public class Tag {
-
-    private final String name;
-
-    public Tag(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tag tag = (Tag) o;
-        return Objects.equals(name, tag.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
 }
