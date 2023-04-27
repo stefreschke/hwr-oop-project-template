1e312a269aaa602f533535ca0d3726fd06b9e917
ChristianSteiner <cs.christian.steiner@gmail.com>
2023-04-27 10:17:39 +0200

    deleted addTableTags, addTableDatabase, addTableProjects || added initTables

--- src/main/java/hwr/oop/application/Database.java
+++ src/main/java/hwr/oop/application/Database.java
@@ -16,108 +16,20 @@ public class Database {
         }
         return conn;
     }
-        /**
-         * Connect to database
-         *
-         */
-        //createNewDatabase
-        public static void createNewDatabase() {
+        public static void main(String[] args){
+            initTables();
+        }
 
-            //url = "jdbc:sqlite:src/main/java/hwr/oop/database/" + fileName;
+        public static void initTables() {
+            String url = "jdbc:sqlite:/path/to/database.db"; // Replace with the path to your SQLite database file
+            String sqlFile = "/path/to/sql/file.sql"; // Replace with the path to your SQL file
 
-            try (Connection conn = DriverManager.getConnection(System.getenv("DATABASE_FILE_URL"))) {
-                if (conn != null) {
-                    DatabaseMetaData meta = conn.getMetaData();
-                    System.out.println("The driver name is " + meta.getDriverName());
-                    System.out.println("A new database has been created.");
-                }
-
-            } catch (SQLException e) {
+            try (Connection conn = DriverManager.getConnection(System.getenv("DATABASE_FILE_URL"));
+                 Statement stmt = conn.createStatement()) {
+                String sql = new String(Files.readAllBytes(Paths.get(System.getenv("SQL_FILE_URL"))));
+                stmt.executeUpdate(sql);
+            } catch (SQLException | IOException e) {
                 System.out.println(e.getMessage());
             }
         }
-
-        public void insert(String tableName, String tableCol){
-            String sql = "INSERT INTO " +tableName +;
-            try (Connection conn = this.connect();
-                 PreparedStatement pstmt = conn.prepareStatement(sql)){
-
-            }
-        }
-    public static void createTableTags() {
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS tags (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	name text NOT NULL,\n"
-                + "	discription text \n"
-                + ");";
-
-        try (Connection conn = DriverManager.getConnection(System.getenv("DATABASE_FILE_URL"));
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public static void createTableProjects() {
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS projects (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	name text NOT NULL,\n"
-                + "	discription text, \n"
-                + " dateTimeCreated , \n"
-                + " dateTimeDeadLine text \n"
-                + ");";
-
-        try (Connection conn = DriverManager.getConnection(System.getenv("DATABASE_FILE_URL"));
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-    public static void createTableTasks() {
-        // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS tasks (\n"
-                + "	id integer PRIMARY KEY,\n"
-                + "	title text NOT NULL,\n"
-                + "	discription text, \n"
-                + " status text, \n"
-                + " dateTimeCreated text, \n"
-                + " dateTimeDone text, \n"
-                + " dateTimePlannedStart text, \n"
-                + " dateTimeDeadLine text \n"
-                + ");";
-
-        try (Connection conn = DriverManager.getConnection(System.getenv("DATABASE_FILE_URL"));
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        }
-    public static void main (String []args){
-            createNewDatabase();
-            createTableTasks();
-            createTableTags();
-            createTableProjects();
-    }
-
-    public static void initTables() {
-        String url = "jdbc:sqlite:/path/to/database.db"; // Replace with the path to your SQLite database file
-        String sqlFile = "/path/to/sql/file.sql"; // Replace with the path to your SQL file
-
-        try (Connection conn = DriverManager.getConnection(System.getenv("DATABASE_FILE_URL"));
-             Statement stmt = conn.createStatement()) {
-            String sql = new String(Files.readAllBytes(Paths.get(System.getenv("SQL_FILE_URL"))));
-            stmt.executeUpdate(sql);
-        } catch (SQLException | IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
-
