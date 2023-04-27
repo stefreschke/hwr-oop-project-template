3c18b4c1514b92a0c275b05eb98769270f28ed94
Marvin Waldmann <marvin.waldmann@sap.com>
2023-04-27 11:46:15 +0200

    FileAdapterTest.save implemented

--- src/test/java/hwr/oop/group4/todo/persistence/FileAdapterTest.java
+++ src/test/java/hwr/oop/group4/todo/persistence/FileAdapterTest.java
@@ -1,26 +1,33 @@
 package hwr.oop.group4.todo.persistence;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class FileAdapterTest {
 
     @Test
-    void save(@TempDir Path path) {
+    void save(@TempDir Path tempDir) {
         Persisted data = mock();
         when(data.toString()).thenReturn("demo file content");
 
-        File file = new File(path.toString() + "/FileAdapterTest.save.txt");
-        System.out.println(file.getPath());
+        Path path = Path.of(tempDir.toString() + "/FileAdapterTest.save.txt");
+        File file = new File(path.toUri());
 
         FileAdapter fileAdapter = new FileAdapter(file);
         fileAdapter.save(data);
+
+        assertThat(Files.exists(path)).isTrue();
+        assertThat(file).hasContent("demo file content");
     }
 
     @Test
