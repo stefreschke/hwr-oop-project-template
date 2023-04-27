f16cb8c7010b88a261352e00d9f5ddb0022b777e
Jonas Liendl <57270170+JonasLiendl@users.noreply.github.com>
2023-04-27 09:35:48 +0200

    Added method to init database tables

--- src/main/java/hwr/oop/application/Database.java
+++ src/main/java/hwr/oop/application/Database.java
@@ -1,4 +1,7 @@
 package hwr.oop.application;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class Database {
@@ -102,7 +105,19 @@ public class Database {
             createTableTasks();
             createTableTags();
             createTableProjects();
+    }
 
+    public static void initTables() {
+        String url = "jdbc:sqlite:/path/to/database.db"; // Replace with the path to your SQLite database file
+        String sqlFile = "/path/to/sql/file.sql"; // Replace with the path to your SQL file
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            String sql = new String(Files.readAllBytes(Paths.get(sqlFile)));
+            stmt.executeUpdate(sql);
+        } catch (SQLException | IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
