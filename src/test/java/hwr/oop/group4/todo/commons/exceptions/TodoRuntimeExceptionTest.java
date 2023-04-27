c0008daacc85c2843aee5d412505f57ccb255ed4
Caroline_Schaefer <caroline.schaefer@sap.com>
2023-04-27 11:30:11 +0200

    optimize line coverage RunTimeException

---
+++ src/test/java/hwr/oop/group4/todo/commons/exceptions/TodoRuntimeExceptionTest.java
@@ -0,0 +1,32 @@
+package hwr.oop.group4.todo.commons.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TodoRuntimeExceptionTest {
+
+    @Test
+    void runTimeExceptionNothing(){
+
+        assertThat(new TodoRuntimeException()).hasMessage(null).hasCause(null);
+    }
+
+    @Test
+    void runTimeException(){
+
+        assertThat(new TodoRuntimeException("sjs")).hasMessage("sjs").hasCause(null);
+    }
+
+    @Test
+    void runTimeExceptionWithCause(){
+        assertThat(new TodoRuntimeException("test", null)).hasMessage("test").hasCause(null);
+    }
+
+    @Test
+    void runTimeExceptionAll() {
+        assertThat(new TodoRuntimeException("test", null, true, true)).hasMessage("test").hasCause(null);
+
+    }
+
+}
---
+++ src/test/java/hwr/oop/group4/todo/commons/exceptions/TodoUiRuntimeExceptionTest.java
@@ -0,0 +1,30 @@
+package hwr.oop.group4.todo.commons.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TodoUiRuntimeExceptionTest {
+    @Test
+    void runTimeExceptionNothing(){
+
+        assertThat(new TodoUiRuntimeException()).hasMessage(null).hasCause(null);
+    }
+
+    @Test
+    void runTimeException(){
+
+        assertThat(new TodoUiRuntimeException("sjs")).hasMessage("sjs").hasCause(null);
+    }
+
+    @Test
+    void runTimeExceptionWithCause(){
+        assertThat(new TodoUiRuntimeException("test", null)).hasMessage("test").hasCause(null);
+    }
+
+    @Test
+    void runTimeExceptionAll() {
+        assertThat(new TodoUiRuntimeException("test", null, true, true)).hasMessage("test").hasCause(null);
+
+    }
+}
