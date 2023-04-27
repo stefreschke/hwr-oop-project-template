edcf6e6e02df2acf388a972161dbcc012d0dca7a
Marvin Waldmann <marvin.waldmann@sap.com>
2023-04-27 10:25:19 +0200

    FileAdapterTest: save file with demo content

--- src/test/java/hwr/oop/group4/todo/persistence/FileAdapterTest.java
+++ src/test/java/hwr/oop/group4/todo/persistence/FileAdapterTest.java
@@ -1,13 +1,27 @@
 package hwr.oop.group4.todo.persistence;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class FileAdapterTest {
 
     @Test
-    void save() {
+    void save(@TempDir Path path) {
+        Persisted data = mock();
+        when(data.toString()).thenReturn("demo file content");
+
+        File file = new File(path.toString() + "/FileAdapterTest.save.txt");
+        System.out.println(file.getPath());
+
+        FileAdapter fileAdapter = new FileAdapter(file);
+        fileAdapter.save(data);
     }
 
     @Test
