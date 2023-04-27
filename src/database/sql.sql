d5d003e9e8ebe34069cae027e154f539a0f371ad
ChristianSteiner <cs.christian.steiner@gmail.com>
2023-04-27 10:40:58 +0200

    configured conn = null, added database.run.xml with enviromental variables

---
+++ src/database/sql.sql
@@ -0,0 +1,66 @@
+create table users (
+    id int primary key not null,
+    'name' varchar(50) not null,
+    'password' varchar(200) not null
+)
+;
+
+
+create table tasks
+(
+    id                   int primary key not null,
+    description          varchar(500),
+    status               int             not null,
+    priority             int             not null,
+    dateTimeCreated      text            not null,
+    dateTimeDone         text,
+    dateTimePlannedStart text,
+    dateTimePlannedEnd   text,
+    dateTimeDeadline     text,
+    projectId            int,
+	foreign key (projectId) REFERENCES projects(id)
+)
+;
+
+create table assigned_to_task
+(
+    userId int, 
+    taskId int, 
+	foreign key (userId) REFERENCES users(id),
+	foreign key (taskId) REFERENCES tasks(id)
+)
+;
+
+create table tagged
+(
+    taskId int,
+	tagId  int,
+	foreign key (taskId) REFERENCES tasks(id),
+	FOREIGN key (tagId) REFERENCES tags(id)
+)
+;
+create table assigned_to_projects
+(
+    userId    int,
+	projectId int, 
+	foreign key (userId) REFERENCES users(id),
+	foreign key (projectId) REFERENCES projects(id)
+)
+;
+create table projects
+(
+    id               int primary key not null,
+    title            varchar(100)    not null,
+    description      varchar(500),
+    dateTimeCreated  text            not null,
+    dateTimeDeadline text            not null,
+    ownerId          int,
+	foreign key (ownerId) REFERENCES owner(id)
+)
+;
+create table tags
+(
+    id    int primary key not null,
+    title varchar(100)    not null
+)
+;
