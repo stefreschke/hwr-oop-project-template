a494e582f18133783b16d75cee246bdfcba77722
Justus <justus.winter@urbanandmainlines.com>
2023-04-27 09:35:36 +0200

    clean up

--- src/main/java/hwr/oop/todo/IOController.java
+++ src/main/java/hwr/oop/todo/IOController.java
@@ -6,34 +6,26 @@ import java.util.Scanner;
 
 public class IOController {
 
-
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     Scanner scanner;
 
     public IOController (InputStream in){
         this.scanner = new Scanner(in);
     }
+    public IOController () { this.scanner = new Scanner(System.in);}
+
     public void printPrompt(String prompt) {
+        System.out.println("\033[2J");
+
         int textLength = prompt.length();
-        String line = "";
-        for (int i = 0; i < (int)(textLength * 1.0) + 2; i++) {
-            line += "-";
-        }
+        StringBuilder line = new StringBuilder();
+        line.append("-".repeat(Math.max(0, (int) (textLength * 1.0) + 2)));
+
         System.out.println("|" + line + "|");
         System.out.println("| " + prompt + " |");
         System.out.println("|" + line + "|");
+        System.out.println("\u001B[32m");
+        System.out.print("> ");
     }
-
-    // TODO: print question
 
     public String getInputString(){
         return this.scanner.nextLine();
