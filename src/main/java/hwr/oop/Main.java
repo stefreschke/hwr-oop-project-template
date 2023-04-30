3985916b31fbd3a83d6f15a85a67909dde49d2bc
Lukas Klockenhoff <lukasklockenhoff@icloud.com>
2023-04-30 11:13:52 +0200

    handleSort function to reduce ifelseifelse...

--- src/main/java/hwr/oop/Main.java
+++ src/main/java/hwr/oop/Main.java
@@ -259,6 +259,35 @@ public class Main {
         out.println("  help     - print this help");
     }
 
+    private static void handleSort(List list, String[] commandArray) {
+        int nCommands = commandArray.length;
+        if (nCommands == 2) {
+            sortHelp();
+            return;
+        } else if (nCommands == 3) {
+            if (commandArray[2].equals("help")) {
+                sortHelp();
+                return;
+            } else if (commandArray[2].toLowerCase().contains("tag")) {
+                list.bubbleUpTag(commandArray[3]);
+            }
+        } else if (nCommands == 4) {
+            if (commandArray[2].toLowerCase().contains("prio")) {
+                if (commandArray[3].equals("asc")) {
+                    list.sortByPriority("asc");
+                } else {
+                    list.sortByPriority("desc");
+                }
+            } else if (commandArray[2].toLowerCase().contains("create")) {
+                if (commandArray[3].equals("asc")) {
+                    list.sortByCreatedAt("asc");
+                } else {
+                    list.sortByCreatedAt("desc");
+                }
+            }
+        }
+    }
+
     private static void clear(List list) {
         list.setListToDos(null);
     }
@@ -296,27 +325,7 @@ public class Main {
                         error("Try 'gtd edit [index]'");
                     }
                 } else if (commandArray[1].equalsIgnoreCase("sort")) {
-                    try {
-                        if (commandArray[2].toLowerCase().contains("prio")) {
-                            if (commandArray[3].equals("asc")) {
-                                toDoList.sortByPriority("asc");
-                            } else {
-                                toDoList.sortByPriority("desc");
-                            }
-                        } else if (commandArray[2].toLowerCase().contains("create")) {
-                            if (commandArray[3].equals("asc")) {
-                                toDoList.sortByCreatedAt("asc");
-                            } else {
-                                toDoList.sortByCreatedAt("desc");
-                            }
-                        } else if (commandArray[2].toLowerCase().contains("tag")) {
-                            toDoList.bubbleUpTag(commandArray[3]);
-                        } else if (commandArray[2].equalsIgnoreCase("help")) {
-                            sortHelp();
-                        } else sortHelp();
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        sortHelp();
-                    }
+                    handleSort(toDoList, commandArray);
                 } else if (commandArray[1].equals("clear")) {
                     clear(toDoList);
                 } else if (commandArray[1].equals("exit")) {
