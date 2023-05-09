3ea5d6e5226392ca774fce29a4be78ebaba6a99d
lenseB <lenseB>
2023-05-09 12:58:49 +0200

    Test for card drawing in Trainer

--- src/test/java/hwr/oop/cards/TrainerTests.java
+++ src/test/java/hwr/oop/cards/TrainerTests.java
@@ -17,7 +17,7 @@ public class TrainerTests {
         Assertions.assertThat(length).isEqualTo(3);
     }
     @Test
-    void canLoadTopic(){
+    void canLoadTopic_OneCard(){
         Trainer trainer = new Trainer();
         Topic topic = new Topic("Spanish");
         topic.createCard("Marco", "Polo");
@@ -25,5 +25,24 @@ public class TrainerTests {
         Card cardRef1 = topic.getCardList().get(0);
         Card cardRef2 = trainer.getBoxList().get(0).getRandomCard();
         Assertions.assertThat(cardRef1).isEqualTo(cardRef2);
+    }
+    @Test
+    void canLoadTopic_ThreeCards(){
+        Trainer trainer = new Trainer();
+        Topic topic = new Topic("Spanish");
+        topic.createCard("Marco", "Polo");
+        topic.createCard("Julius", "Caesar");
+        topic.createCard("Freddy", "Faulig");
+        trainer.loadTopic(topic);
+        Card cardRef1_topic = topic.getCardList().get(0);
+        Card card1_rand = trainer.getBoxList().get(0).getRandomCard();
+        Assertions.assertThat(topic.getCardList()).contains(card1_rand);
+        Card cardRef2_topic = topic.getCardList().get(1);
+        Card card2_rand = trainer.getBoxList().get(0).getRandomCard();        Assertions.assertThat(topic.getCardList()).contains(card1_rand);
+        Assertions.assertThat(topic.getCardList()).contains(card2_rand);
+        Card cardRef3_topic = topic.getCardList().get(2);
+        Card card3_rand = trainer.getBoxList().get(0).getRandomCard();
+        Assertions.assertThat(topic.getCardList()).contains(card3_rand);
+
     }
 }
