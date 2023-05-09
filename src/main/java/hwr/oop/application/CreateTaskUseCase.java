1d47ffbfad8de73d3d0671fc080e509d89344315
Johannes <Johannes.Foltin@gmx.de>
2023-05-09 14:08:29 +0200

    deleted old Application.java changed Id from Task to UUID

--- src/main/java/hwr/oop/application/CreateTaskUseCase.java
+++ src/main/java/hwr/oop/application/CreateTaskUseCase.java
@@ -4,6 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CreateTaskUseCase {
-    void createTask(int id, String title, String content, TaskState state, List<TaskTag> tagList, User creator,
+    void createTask(String title, String content, TaskState state, List<TaskTag> tagList, User creator,
                     LocalDateTime deadline);
 }
