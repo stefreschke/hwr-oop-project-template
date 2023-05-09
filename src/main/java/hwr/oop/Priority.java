ad521a08a85b233201b54954f032fc7f32015feb
EmmaLouis222 <124029682+EmmaLouis222@users.noreply.github.com>
2023-05-09 10:53:21 +0200

    changes

--- src/main/java/hwr/oop/Priority.java
+++ src/main/java/hwr/oop/Priority.java
@@ -5,11 +5,13 @@ public enum Priority {
     MEDIUM(1),
     HIGH(2);
 
+
+    private int value;
+
     Priority(int value) {
         this.value = value;
     }
 
-    private int value;
     public int toInt() {
         return value;
     }
