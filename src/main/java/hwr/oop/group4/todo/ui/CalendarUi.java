99ba7616321cd1606f50a2149d9337ef8f5fc7e1
Vin1X <vincent.walura@hotmail.com>
2023-05-20 00:04:17 +0200

    added few comments for structure

--- src/main/java/hwr/oop/group4/todo/ui/CalendarUi.java
+++ src/main/java/hwr/oop/group4/todo/ui/CalendarUi.java
@@ -14,17 +14,17 @@ import java.util.List;
 
 public class CalendarUi {
     private final ConsoleController consoleController;
-    //private final ConsoleHelper consoleHelper;
+    private final ConsoleHelper consoleHelper;
     private TodoList todoList;
     private final String[] daysOfWeek = {"Mo","Di","Mi","Do","Fr","Sa","So"};
 
     public CalendarUi(ConsoleController consoleController){
         this.consoleController = consoleController;
-        //this.consoleHelper = new consoleHelper();
+        this.consoleHelper = new ConsoleHelper();
     }
-    public void menu(TodoList todoList){
+    public void menu(TodoList todoList){ //enough entries?
         this.todoList = todoList;
-        Menu menu = new Menu("Calendar Menu", "just try", List.of(
+        Menu menu = new Menu("Calendar Menu", "See your current Calendar?", List.of(
             new Entry("next week", "Scroll to the next week"),
             new Entry("last week", "Scroll one week back"),
             new Entry("back", "Return to the previous menu")
@@ -41,7 +41,7 @@ public class CalendarUi {
             System.out.print(String.format("%10s", day));
         }
         System.out.println();
-        for (int i = 0; i < 7; i++){
+        for (int i = 0; i < 7; i++){ //create with table class
             int day = calendar.get(Calendar.DAY_OF_MONTH);
             System.out.print(String.format("%10d", day));
             calendar.add(Calendar.DAY_OF_MONTH, 1);
@@ -49,8 +49,13 @@ public class CalendarUi {
         System.out.println();
     }
 
-    private void assignTask(Task task){
+    //create 2 funcs for scrolling through different weeks?(+/- 7d)
 
-        Task.getTag();
+    private void assignTaskToCalendar(Task task){
+        //get deadline from a task and assign it to the date
+        //while 1
+        // Task.getDate
+        // if deadline = currentDate
+        // add to day
     }
 }
