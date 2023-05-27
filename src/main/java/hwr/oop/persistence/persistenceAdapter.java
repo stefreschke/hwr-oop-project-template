ad235cb14171348c7faad2d46c954a900322c1aa
Julian-He <heruthjulian@gmail.com>
2023-05-27 20:15:21 +0200

    added persistenceAdapter

---
+++ src/main/java/hwr/oop/persistence/persistenceAdapter.java
@@ -0,0 +1,13 @@
+package hwr.oop.persistence;
+
+public class persistenceAdapter implements LoadPort, SavePort{
+    @Override
+    public AppData loadData() {
+        return null;
+    }
+
+    @Override
+    public void saveData(AppData appData) {
+
+    }
+}
