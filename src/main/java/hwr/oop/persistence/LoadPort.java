23634b09be04ba7819d075ff99cc1aa2ff92856e
Julian-He <heruthjulian@gmail.com>
2023-05-30 13:04:52 +0200

    added syntactic sugar methods to loadPort

--- src/main/java/hwr/oop/persistence/LoadPort.java
+++ src/main/java/hwr/oop/persistence/LoadPort.java
@@ -1,8 +1,19 @@
 package hwr.oop.persistence;
 
+import hwr.oop.application.Project;
+import hwr.oop.application.User;
 import java.io.FileNotFoundException;
 import java.io.Reader;
+import java.util.List;
+import java.util.UUID;
 
 public interface LoadPort {
     AppData loadData(Reader fileReader) throws FileNotFoundException;
+
+    Project loadProjectById(Reader fileReader, UUID projectID) throws FileNotFoundException;
+
+    User loadUserbyId(Reader fileReader, UUID userId) throws FileNotFoundException;
+
+    List<Project> loadAllUserProjects(Reader fileReader, UUID userId) throws FileNotFoundException;
+    //takes user and gives back all projects this user is involved in
 }
--- src/main/java/hwr/oop/persistence/PersistenceAdapter.java
+++ src/main/java/hwr/oop/persistence/PersistenceAdapter.java
@@ -2,9 +2,15 @@ package hwr.oop.persistence;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import hwr.oop.application.Project;
+import hwr.oop.application.User;
+
+import java.io.FileNotFoundException;
 import java.io.Reader;
 import java.io.Writer;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 public class PersistenceAdapter implements LoadPort, SavePort {
     @Override
@@ -13,6 +19,21 @@ public class PersistenceAdapter implements LoadPort, SavePort {
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
                 .create();
         return gson.fromJson(fileReader, AppData.class);
+    }
+
+    @Override
+    public Project loadProjectById(Reader fileReader, UUID projectID) throws FileNotFoundException {
+        return null;
+    }
+
+    @Override
+    public User loadUserbyId(Reader fileReader, UUID userId) throws FileNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<Project> loadAllUserProjects(Reader fileReader, UUID userId) throws FileNotFoundException {
+        return null;
     }
 
     @Override
