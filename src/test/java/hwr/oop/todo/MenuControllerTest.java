fb2331c054d07f2965356fa5c37eb159863e9628
broemme_m1 <moritz.broemme@biotronik.com>
2023-05-31 16:16:58 +0200

    MenuControllerTest, added mockito dependency

---
+++ src/test/java/hwr/oop/todo/MenuControllerTest.java
@@ -0,0 +1,40 @@
+package hwr.oop.todo;
+
+import hwr.oop.todo.io.IOController;
+import hwr.oop.todo.library.todolist.ToDoList;
+import hwr.oop.todo.ui.MenuController;
+import hwr.oop.todo.ui.menu.Menu;
+import hwr.oop.todo.ui.menu.responses.MenuResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class MenuControllerTest {
+
+    private ToDoList mockToDoList;
+    private IOController mockIOController;
+    private MenuController menuController;
+
+    @BeforeEach
+    void setUp() {
+        mockToDoList = mock(ToDoList.class);
+        mockIOController = mock(IOController.class);
+        menuController = new MenuController(mockToDoList, mockIOController);
+    }
+
+    @Test
+    void testAskForParameter() {
+        String parameterName = "testParameter";
+        String expectedInput = "Test Input";
+        when(mockIOController.getInputString()).thenReturn(expectedInput);
+
+        String result = menuController.askForParameter(parameterName);
+
+        verify(mockIOController).printPrompt(parameterName + "?");
+        verify(mockIOController).getInputString();
+        assertEquals(expectedInput, result);
+    }
+
+}
+
