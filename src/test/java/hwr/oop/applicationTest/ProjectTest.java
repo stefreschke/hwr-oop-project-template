c9e72d2c2605de662a1cf9ddf975fd2238b98d29
Julian-He <heruthjulian@gmail.com>
2023-06-01 23:38:07 +0200

    changed method name

--- src/test/java/hwr/oop/applicationTest/ProjectTest.java
+++ src/test/java/hwr/oop/applicationTest/ProjectTest.java
@@ -6,6 +6,8 @@ import hwr.oop.persistence.AppData;
 import hwr.oop.persistence.LoadPort;
 import hwr.oop.persistence.PersistenceAdapter;
 import hwr.oop.persistence.SavePort;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -19,15 +21,16 @@ class ProjectTest {
     LoadPort load = new PersistenceAdapter("./OOPTest");
     SavePort save = new PersistenceAdapter("./OOPTest");
 
-    static Stream<Arguments> randomProject() {
-        return Stream.of(Arguments.of(RandomTestData.getRandomProject()));
+    static Stream<Arguments> randomProjects() {
+        return Stream.of(Arguments.of(RandomTestData.getRandomProjects()));
     }
 
     @ParameterizedTest
-    @MethodSource("randomProject")
+    @MethodSource("randomProjects")
     void canCreateProject(Project expected) {
         AppData appData = new AppData(new ArrayList<>(), new ArrayList<>());
         projectCreation.createProject(appData, expected.getTitle(), expected.getTaskList(), expected.getPermissions());
         assertThat(load.loadData().getProjectList().get(0)).isEqualTo(expected);
     }
+
 }
