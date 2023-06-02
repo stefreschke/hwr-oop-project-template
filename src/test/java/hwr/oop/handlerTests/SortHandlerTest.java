26aac4e36094c155e9c1093fa148da0625345bb3
Sander Hahn <sander.hahn@helmholtz-berlin.de>
2023-06-02 11:00:28 +0200

    fixed minor spelling mistake & added sortByTitle, sortByDone functionality + tests

--- src/test/java/hwr/oop/handlerTests/SortHandlerTest.java
+++ src/test/java/hwr/oop/handlerTests/SortHandlerTest.java
@@ -107,4 +107,100 @@ public class SortHandlerTest {
         assertThat(titles).isEqualTo(new String[]{"Task 4", "Task 3", "Task 2", "Task 1"});
         // Add more assertions to verify the behavior of the add command
     }
+
+    @Test
+    void handleUserCommandTitleAscTest() {
+        // Arrange
+        ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+        String userInput = "Task 1\nDescription 1\nBucket 1\nLOW\n";
+        ConsoleUserInterface cui = new ConsoleUserInterface(new PrintStream(outBuffer), new ByteArrayInputStream(userInput.getBytes(StandardCharsets.UTF_8)));
+        ToDoList toDoList = new ToDoList("MyList", "test.json");
+        toDoList.add(new ToDoItem("Task 1", "Description 1", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 2", "Description 2", "Bucket 1", Priority.LOW));
+        toDoList.add(new ToDoItem("Task 3", "Description 3", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 4", "Description 4", "Bucket 1", Priority.HIGH));
+        String[] args = {"gtd", "sort", "title", "asc"};
+
+        // Act
+        SortHandler.handleUserCommand(toDoList, cui, args);
+        String[] titles = new String[4];
+        for (int i = 0; i < 4; i++) {
+            titles[i] = toDoList.getItems().get(i).getTitle();
+        }
+        // Assert
+        assertThat(titles).isEqualTo(new String[]{"Task 1", "Task 2", "Task 3", "Task 4"});
+        // Add more assertions to verify the behavior of the add command
+    }
+    @Test
+    void handleUserCommandTitleDescTest() {
+        // Arrange
+        ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+        String userInput = "Task 1\nDescription 1\nBucket 1\nLOW\n";
+        ConsoleUserInterface cui = new ConsoleUserInterface(new PrintStream(outBuffer), new ByteArrayInputStream(userInput.getBytes(StandardCharsets.UTF_8)));
+        ToDoList toDoList = new ToDoList("MyList", "test.json");
+        toDoList.add(new ToDoItem("Task 1", "Description 1", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 2", "Description 2", "Bucket 1", Priority.LOW));
+        toDoList.add(new ToDoItem("Task 3", "Description 3", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 4", "Description 4", "Bucket 1", Priority.HIGH));
+        String[] args = {"gtd", "sort", "title", "desc"};
+
+        // Act
+        SortHandler.handleUserCommand(toDoList, cui, args);
+        String[] titles = new String[4];
+        for (int i = 0; i < 4; i++) {
+            titles[i] = toDoList.getItems().get(i).getTitle();
+        }
+        // Assert
+        assertThat(titles).isEqualTo(new String[]{"Task 4", "Task 3", "Task 2", "Task 1"});
+        // Add more assertions to verify the behavior of the add command
+    }
+
+    @Test
+    void handleUserCommandDoneAscTest() {
+        // Arrange
+        ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+        String userInput = "Task 1\nDescription 1\nBucket 1\nLOW\n";
+        ConsoleUserInterface cui = new ConsoleUserInterface(new PrintStream(outBuffer), new ByteArrayInputStream(userInput.getBytes(StandardCharsets.UTF_8)));
+        ToDoList toDoList = new ToDoList("MyList", "test.json");
+        toDoList.add(new ToDoItem("Task 1", "Description 1", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 2", "Description 2", "Bucket 1", Priority.LOW));
+        toDoList.add(new ToDoItem("Task 3", "Description 3", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 4", "Description 4", "Bucket 1", Priority.HIGH));
+        toDoList.getItems().get(1).setDone();
+        String[] args = {"gtd", "sort", "done", "asc"};
+
+        // Act
+        SortHandler.handleUserCommand(toDoList, cui, args);
+        String[] titles = new String[4];
+        for (int i = 0; i < 4; i++) {
+            titles[i] = toDoList.getItems().get(i).getTitle();
+        }
+        // Assert
+        assertThat(titles).isEqualTo(new String[]{"Task 2", "Task 1", "Task 3", "Task 4"});
+        // Add more assertions to verify the behavior of the add command
+    }
+    @Test
+    void handleUserCommandDoneDescTest() {
+        // Arrange
+        ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
+        String userInput = "Task 1\nDescription 1\nBucket 1\nLOW\n";
+        ConsoleUserInterface cui = new ConsoleUserInterface(new PrintStream(outBuffer), new ByteArrayInputStream(userInput.getBytes(StandardCharsets.UTF_8)));
+        ToDoList toDoList = new ToDoList("MyList", "test.json");
+        toDoList.add(new ToDoItem("Task 1", "Description 1", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 2", "Description 2", "Bucket 1", Priority.LOW));
+        toDoList.add(new ToDoItem("Task 3", "Description 3", "Bucket 1", Priority.MEDIUM));
+        toDoList.add(new ToDoItem("Task 4", "Description 4", "Bucket 1", Priority.HIGH));
+        toDoList.getItems().get(1).setDone();
+        String[] args = {"gtd", "sort", "done", "desc"};
+
+        // Act
+        SortHandler.handleUserCommand(toDoList, cui, args);
+        String[] titles = new String[4];
+        for (int i = 0; i < 4; i++) {
+            titles[i] = toDoList.getItems().get(i).getTitle();
+        }
+        // Assert
+        assertThat(titles).isEqualTo(new String[]{"Task 1", "Task 3", "Task 4", "Task 2"});
+        // Add more assertions to verify the behavior of the add command
+    }
 }
