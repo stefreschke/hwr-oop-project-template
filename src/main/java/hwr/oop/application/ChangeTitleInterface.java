8f03a3c280ff4dff5f323859916b6ce322620cb9
Sofie Wagner <sofie.wagner@helmholtz-berlin.de>
2023-06-02 14:39:49 +0200

    changeTitle in interface and service

--- src/main/java/hwr/oop/application/ChangeTitleInterface.java
+++ src/main/java/hwr/oop/application/ChangeTitleInterface.java
@@ -1,4 +1,5 @@
 package hwr.oop.application;
 
 public interface ChangeTitleInterface {
+    void changeTitle(String Title);
 }
--- src/main/java/hwr/oop/application/ChangeTitleService.java
+++ src/main/java/hwr/oop/application/ChangeTitleService.java
@@ -1,4 +1,13 @@
 package hwr.oop.application;
 
-public class ChangeTitleService {
+public class ChangeTitleService implements ChangeTitleInterface{
+    private Project project;
+
+    public ChangeTitleService(Project project) {
+        this.project = project;
+    }
+
+    public void changeTitle(String newTitle){
+        project.changeTitle(newTitle);
+    }
 }
