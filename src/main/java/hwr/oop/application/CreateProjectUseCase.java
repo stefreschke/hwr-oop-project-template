985c08620ae61957277f853f75f13822fe0ddbbb
Julian-He <heruthjulian@gmail.com>
2023-06-04 03:31:12 +0200

    changed Parameters of CreateProjectUseCase

--- src/main/java/hwr/oop/application/CreateProjectUseCase.java
+++ src/main/java/hwr/oop/application/CreateProjectUseCase.java
@@ -1,8 +1,10 @@
 package hwr.oop.application;
 
+import hwr.oop.persistence.AppData;
+import hwr.oop.persistence.SavePort;
 import java.util.List;
 import java.util.Map;
 
 public interface CreateProjectUseCase {
-    void createProject(String title, List<Task> taskList, Map<User, Boolean> permissions);
+    void createProject(SavePort save, AppData appData, String title, List<Task> taskList);
 }
