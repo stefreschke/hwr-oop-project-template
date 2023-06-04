a99431e73c8e7b95f5587a65bd9de547daf19b6f
Julian-He <heruthjulian@gmail.com>
2023-06-04 03:31:59 +0200

    added method to test CreateProjectUseCase with random Projects

--- src/test/java/hwr/oop/applicationTest/ProjectTest.java
+++ src/test/java/hwr/oop/applicationTest/ProjectTest.java
@@ -10,8 +10,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -21,13 +27,13 @@ class ProjectTest {
     AppData appData;
     CreateProjectUseCase createProject;
     LoadPort load;
+    SavePort save;
 
     @BeforeEach
     void setUp() {
         appData = new AppData(new ArrayList<>(), new ArrayList<>());
         load = new PersistenceAdapter(directory);
         save = new PersistenceAdapter(directory);
-
         File file = new File(directory + AppData.class + ".json");
         if (file.exists()) {
             file.delete();
@@ -46,11 +52,26 @@ class ProjectTest {
         );
     }
 
+    static Stream<Arguments> randomProjectsWithSingleUser() {
+        List<Arguments> randomProjects = new ArrayList<>();
+        for (int i=0; i<5; i++) {
+            Project rando = RandomTestData.getRandomProject();
+            randomProjects.add(
+                    Arguments.of(
+                            new Project(rando.getId(), rando.getTaskList(), rando.getTitle(),
+                                    Map.of(RandomTestData.getRandomUser(), Boolean.TRUE)
+                            )
+                    )
+            );
+        }
+        return  randomProjects.stream();
+    }
+
     @ParameterizedTest
-    @MethodSource("randomProjects")
+    @MethodSource("randomProjectsWithSingleUser")
     void canCreateProject(Project expected) {
         AppData appData = new AppData(new ArrayList<>(), new ArrayList<>());
-        createProject.createProject(appData, expected.getTitle(), expected.getTaskList(), expected.getPermissions());
+        createProject.createProject(save, appData, expected.getTitle(), expected.getTaskList());
         assertThat(load.loadData().getProjectList().get(0)).isEqualTo(expected);
     }
 
