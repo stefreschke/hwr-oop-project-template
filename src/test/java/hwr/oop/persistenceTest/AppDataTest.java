5058ef1a2d93db79b331dae0f56c62f790876ca8
Julian-He <heruthjulian@gmail.com>
2023-06-05 14:42:39 +0200

    added Tests for PersistenceAdapter-Exceptions

--- src/test/java/hwr/oop/persistenceTest/AppDataTest.java
+++ src/test/java/hwr/oop/persistenceTest/AppDataTest.java
@@ -1,15 +1,39 @@
 package hwr.oop.persistenceTest;
 
+import hwr.oop.application.CreateProjectService;
+import hwr.oop.application.CreateProjectUseCase;
 import hwr.oop.applicationTest.RandomTestData;
-import hwr.oop.persistence.AppData;
+import hwr.oop.persistence.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.*;
 
 class AppDataTest {
 
+    String directory = "./OOPTest/";
+    AppData appData;
+    LoadPort load;
+    SavePort save;
+
+    @BeforeEach
+    void setUp() {
+        appData = new AppData(new ArrayList<>(), new ArrayList<>());
+        load = new PersistenceAdapter(directory);
+        save = new PersistenceAdapter(directory);
+
+        final File parent = new File(directory);
+        parent.mkdirs();
+        File file = new File(directory + AppData.class + ".json");
+        file.delete();
+    }
+
     @Test
     void equalsTest() {
-        AppData appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
+        appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
         AppData copy = new AppData(appData.getProjectList(), appData.getUserList());
         assertThat(appData).isEqualTo(copy);
         assertThat(copy).isEqualTo(appData);
@@ -18,13 +42,13 @@ class AppDataTest {
 
     @Test
     void equalsExactlyTest() {
-        AppData appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
+        appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
         assertThat(appData).isEqualTo(appData);
     }
 
     @Test
     void differentProjectList_doesNotEqual() {
-        AppData appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
+        appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
         AppData copy = new AppData(RandomTestData.getRandomProjects(), appData.getUserList());
         assertThat(appData).isNotEqualTo(copy);
         assertThat(copy).isNotEqualTo(appData);
@@ -32,8 +56,33 @@ class AppDataTest {
 
     @Test
     void differentClass_doesNotEqual() {
-        AppData appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
+        appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
         assertThat(appData).isNotEqualTo(new Object());
         assertThat(new Object()).isNotEqualTo(appData);
+    }
+
+    @Test
+    void wrongFile_ThrowsLoadException() {
+        appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
+        save.saveData(appData);
+        load = new PersistenceAdapter("./wrongPath");
+        try {
+            AppData result = load.loadData();
+            fail("should throw Exception");
+        } catch (CantLoadAppDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void wrongFile_ThrowsSaveException() {
+        appData = new AppData(RandomTestData.getRandomProjects(), RandomTestData.getRandomUsers());
+        save = new PersistenceAdapter("/wrongPath//");
+        try {
+            save.saveData(appData);
+            fail("should throw exception");
+        } catch (CantSaveAppDataException e) {
+            e.printStackTrace();
+        }
     }
 }
