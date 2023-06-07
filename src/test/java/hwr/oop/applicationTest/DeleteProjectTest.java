7f8ac0b24b3288b2f458261fec7f733e1336d372
Julian-He <heruthjulian@gmail.com>
2023-06-07 17:04:06 +0200

    added setUp()

--- src/test/java/hwr/oop/applicationTest/DeleteProjectTest.java
+++ src/test/java/hwr/oop/applicationTest/DeleteProjectTest.java
@@ -1,10 +1,14 @@
 package hwr.oop.applicationTest;
 
+import hwr.oop.application.DeleteProjectService;
 import hwr.oop.application.DeleteProjectUseCase;
 import hwr.oop.persistence.LoadPort;
 import hwr.oop.persistence.PersistenceAdapter;
 import hwr.oop.persistence.SavePort;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+import java.io.IOException;
 
 public class DeleteProjectTest {
 
@@ -12,4 +16,21 @@ public class DeleteProjectTest {
     LoadPort load;
     SavePort save;
     DeleteProjectUseCase deleteProject;
+
+    @BeforeEach
+    void setUp() {
+        load = new PersistenceAdapter(directory);
+        save = new PersistenceAdapter(directory);
+        deleteProject = new DeleteProjectService(load, save);
+
+        final File parent = new File(directory);
+        parent.mkdirs();
+        File file = new File(directory + "AppData.json");
+        file.delete();
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
