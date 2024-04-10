57fe96832c0880ce3a78ffcb4b0351b34c3e96c4
lagopodus <149696434+lagopodus@users.noreply.github.com>
2024-04-10 10:08:45 +0200

    Update Cards.java

--- src/main/java/hwr/oop/Cards.java
+++ src/main/java/hwr/oop/Cards.java
@@ -1,23 +1,26 @@
 package hwr.oop;
 
 public class Cards {
-
-    public static String[] generateCards() {
+    public static Card[] generateCards() {
         String[] symbols = {"9", "10", "J", "Q", "K", "A"};
         String[] colours = {"H", "D", "C", "S"};// Heart, Diamonds, Clubs, Spades
+        int[] values = {0, 10, 2, 3, 4, 11};
 
-        String[] generatedCards = new String[symbols.length * colours.length * 2];
+        Card[] generatedCards = new Card[symbols.length * colours.length * 2];
         int index = 0;
         for (String symbol : symbols) {
+            int value = values[0];
             for (String colour : colours) {
-                String cardName = colour + symbol;
-                generatedCards[index++] = cardName;
-                generatedCards[index++] = cardName;
+                    Card card = new Card(symbol, colour, value);
+                    generatedCards[index++] = card;
+                    generatedCards[index++] = card;
             }
         }
         return generatedCards;
     }
 }
+
+
 
 
 
