7395dfce337e40c815c322ee6d14d8008b78ae4c
colin <colin.handro@gmail.com>
2024-04-10 09:20:55 +0200

    comment on card colours

--- src/main/java/hwr/oop/Cards.java
+++ src/main/java/hwr/oop/Cards.java
@@ -4,7 +4,7 @@ public class Cards {
 
     public static String[] generateCards() {
         String[] symbols = {"9", "10", "J", "Q", "K", "A"};
-        String[] colours = {"H", "D", "C", "S"};
+        String[] colours = {"H", "D", "C", "S"};// Heart, Diamonds, Clubs, Spades
 
         String[] generatedCards = new String[symbols.length * colours.length * 2];
         int index = 0;
