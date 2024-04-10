ea876232bbf8cc42eeb668d149c02d2057df4cdd
joshnicot06 <j.tischer@fu-berlin.de>
2024-04-10 10:05:51 +0200

    Cards are now added as object of class card

--- src/main/java/hwr/oop/Cards.java
+++ src/main/java/hwr/oop/Cards.java
@@ -3,7 +3,7 @@ package hwr.oop;
 public class Cards {
     public static Card[] generateCards() {
         String[] symbols = {"9", "10", "J", "Q", "K", "A"};
-        String[] colours = {"H", "D", "C", "S"};
+        String[] colours = {"H", "D", "C", "S"};// Heart, Diamonds, Clubs, Spades
         int[] values = {0, 10, 2, 3, 4, 11};
 
         Card[] generatedCards = new Card[symbols.length * colours.length * 2];
