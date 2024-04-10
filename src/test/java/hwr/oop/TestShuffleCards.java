fd3d83d549a757b9313e0bc5ac65d2bc79335c79
colin <colin.handro@gmail.com>
2024-04-10 12:24:15 +0200

    first test designed but not ready

---
+++ src/test/java/hwr/oop/TestShuffleCards.java
@@ -0,0 +1,12 @@
+package hwr.oop;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+/*class TestShuffleCards {
+    //scenario : Sorted Card stack will be shuffled
+    @Test
+    void TestShuffleCardStack() {
+        CardStack cardStack = new CardStack();
+        cardStack.shuffleStack();
+    }
+}*/
