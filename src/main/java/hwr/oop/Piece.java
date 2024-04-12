fc3c7d4dcc75f9fc82b4212e51b64cbf7aed2e56
AlexInABox <main@alexinabox.de>
2024-04-12 11:45:44 +0200

    added constructor and get/set methods to piece class

--- src/main/java/hwr/oop/Piece.java
+++ src/main/java/hwr/oop/Piece.java
@@ -1,8 +1,27 @@
 package hwr.oop;
 
-public class Piece {
-  Color color;
-  Square position;
-  boolean captured;
+import hwr.oop.enums.Color;
 
+public class Piece {
+  private final Color color;
+  private final Square position;
+  private boolean captured = false;
+
+  public Piece(Color color, Square position) {
+    this.color = color;
+    this.position = position;
+  }
+
+  public Color getColor() {
+    return color;
+  }
+  public Square getPosition() {
+    return position;
+  }
+  public boolean isCaptured() {
+    return captured;
+  }
+  public void setCaptured(){
+    captured = true;
+  }
 }
