bcdc50ecdb12b992a2785dd46e1626da0e3563ff
Canoobi <carlo.gliech@gmx.de>
2024-04-16 13:21:58 +0200

    update piece enum

--- src/main/java/classes/Piece.java
+++ src/main/java/classes/Piece.java
@@ -7,19 +7,21 @@ public class Piece {
   }
 
   public enum PieceType {
-    BAUER('b', new int[][] {{0, 1}}),
-    TURM('t', new int[][] {{0, 1}, {1, 0}}),
-    SPRINGER('s', new int[][] {{2, 1}, {2, -1}, {1, 2}, {-1, 2}}),
-    LAEUFER('l', new int[][] {{1, 1}, {-1, 1}}),
-    KOENIG('k', new int[][] {{0, 1}, {1, 0}, {1, 1}, {-1, 1}}),
-    DAME('d', new int[][] {{0, 1}, {1, 0}, {1, 1}, {-1, 1}});
+    BAUER('b', new int[][] {{0, 1}}, false),
+    TURM('t', new int[][] {{0, 1}, {1, 0}}, true),
+    SPRINGER('s', new int[][] {{2, 1}, {2, -1}, {1, 2}, {-1, 2}}, false),
+    LAEUFER('l', new int[][] {{1, 1}, {-1, 1}}, true),
+    KOENIG('k', new int[][] {{0, 1}, {1, 0}, {1, 1}, {-1, 1}}, false),
+    DAME('d', new int[][] {{0, 1}, {1, 0}, {1, 1}, {-1, 1}}, true);
 
     private final char abbr;
     private final int[][] moves;
+    private final boolean moveRepeatable;
 
-    PieceType(char abbr, int[][] moves) {
+    PieceType(char abbr, int[][] moves, boolean moveRepeatable) {
       this.abbr = abbr;
       this.moves = moves;
+      this.moveRepeatable = moveRepeatable;
     }
 
     public char getAbbr() {
@@ -29,22 +31,24 @@ public class Piece {
     public int[][] getMoves() {
       return moves;
     }
+
+    public boolean isMoveRepeatable() {
+      return moveRepeatable;
+    }
   }
 
   private int[] actPosition;
   private int[][] posMoves;
+  private boolean moveRepeatable;
   private Color color;
   private char abbr;
 
-  public Piece(int[] pos, int[][] moves, Color color, char abbr) {
+  public Piece(int[] pos, int[][] moves, Color color, char abbr, boolean moveRepeatable) {
     this.actPosition = pos;
     this.posMoves = moves;
+    this.moveRepeatable = moveRepeatable;
     this.color = color;
     this.abbr = abbr;
-  }
-
-  public boolean isValidMove(int x, int y) {
-    return false;
   }
 
   public boolean isBlocked() {
@@ -61,5 +65,17 @@ public class Piece {
 
   public char getAbbr() {
     return this.abbr;
+  }
+
+  public int[] getActPosition() {
+    return this.actPosition;
+  }
+
+  public int[][] getPosMoves() {
+    return this.posMoves;
+  }
+
+  public boolean isMoveRepeatable() {
+    return moveRepeatable;
   }
 }
