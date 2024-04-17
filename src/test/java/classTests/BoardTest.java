05a54f9b60347e188575730a455356e98847cb3d
lisakutemeier <abced1@web.de>
2024-04-17 09:46:36 +0200

    one unit tests for isValidMove function - update

--- src/test/java/classTests/BoardTest.java
+++ src/test/java/classTests/BoardTest.java
@@ -13,15 +13,7 @@ public class BoardTest {
 
         Piece piece = board.getPieceAt(0, 1); // weisser bauer
         assertThat(board.isValidMove(piece, 0, 2)).isTrue(); // bauer bewegt sich eins nach vorn
-    }
-
-    @Test
-    public void testIsValidMoveFail() {
-
-        Board board = new Board();
-        board.initBoard();
-
-        Piece piece = board.getPieceAt(0, 1); // weisser bauer
         assertThat(board.isValidMove(piece, 3, 0)).isFalse(); // bauer kann nicht seitlich gehen
     }
+
 }
