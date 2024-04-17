586b6a95783b907d5a50bb43f575d53c9ba2a466
Luroka <christophleptin@gmail.com>
2024-04-17 14:11:19 +0200

    Created Play-List

--- src/main/java/hwr/oop/Player.java
+++ src/main/java/hwr/oop/Player.java
@@ -1,5 +1,51 @@
 package hwr.oop;
 
+import hwr.oop.enums.TeamNames;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    public  Card[] cards;
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public TeamNames getTeam() {
+        return team;
+    }
+
+    public void setTeam(TeamNames team) {
+        this.team = team;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
+
+    int score;
+    TeamNames team;
+    List<Card> hand = new ArrayList<>();
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
 }
