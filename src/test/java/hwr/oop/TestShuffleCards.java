afc3011bce6991f6ffb1b94f442fb5d15f775040
colin <colin.handro@gmail.com>
2024-04-17 09:58:13 +0200

    Test still not running ):

--- src/test/java/hwr/oop/TestShuffleCards.java
+++ src/test/java/hwr/oop/TestShuffleCards.java
@@ -2,12 +2,19 @@ package hwr.oop;
 import java.util.Random;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class TestShuffleCards {
     //scenario : Sorted Card stack will be shuffled
     @Test
     void TestShuffleCardStack() {
-        CardStack cardStack = new CardStack();
-        cardStack.shuffleStack();
-        assertThat(result).isEqualTo(cardStack)
+        //Given
+        final var cardStack = new CardStack();
+        //When
+        final void shuffledDeck = cardStack.shuffleStack();
+        //Then
+        assertThat(cardStack.cards())
+                .isNotEmpty()
+                .doesNotContainSequence(cardStack);
     }
 }
