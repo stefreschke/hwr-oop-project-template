e8dafe7e9448ba62e1a8650884dcaab7fe7b9909
lisakutemeier <abced1@web.de>
2024-04-17 09:12:14 +0200

    two unit tests for isValidMove function

--- src/test/java/classTests/BoardTest.java
+++ src/test/java/classTests/BoardTest.java
@@ -1,3 +1,27 @@
 package classTests;
 
-public class BoardTest {}
+import classes.Board;
+import classes.Piece;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoardTest {
+    @Test
+    public void testIsValidMoveSuccess() {
+        Board board = new Board();
+        board.initBoard();
+
+        Piece piece = board.getPieceAt(0, 1); // weisser bauer
+        assertThat(board.isValidMove(piece, 0, 2)).isTrue(); // bauer bewegt sich eins nach vorn
+    }
+
+    @Test
+    public void testIsValidMoveFail() {
+
+        Board board = new Board();
+        board.initBoard();
+
+        Piece piece = board.getPieceAt(0, 1); // weisser bauer
+        assertThat(board.isValidMove(piece, 3, 0)).isFalse(); // bauer kann nicht seitlich gehen
+    }
+}
