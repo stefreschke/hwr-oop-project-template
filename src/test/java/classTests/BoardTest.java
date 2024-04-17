ffa0f24d69a94b1ccc96ef815de31a1e64980bf4
Canoobi <carlo.gliech@gmx.de>
2024-04-17 10:03:13 +0200

    fix errors in BoardTest.java

--- src/test/java/classTests/BoardTest.java
+++ src/test/java/classTests/BoardTest.java
@@ -7,10 +7,9 @@ import org.assertj.core.api.Assertions;
 
 public class BoardTest {
     @Test
-    void testIsValidMoveSuccess() {
+    void testIsValidMove() {
         Board board = new Board();
         board.initBoard();
-
         Piece piece = board.getPieceAt(0, 1); // weisser bauer
         Assertions.assertThat(board.isValidMove(piece, 0, 2)).isTrue(); // bauer bewegt sich eins nach vorn
         Assertions.assertThat(board.isValidMove(piece, 3, 0)).isFalse(); // bauer kann nicht seitlich gehen
