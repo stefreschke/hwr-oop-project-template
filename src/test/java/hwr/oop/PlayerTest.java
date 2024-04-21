6c7359b394a8f7270a291d47458eb94227e72958
lukaskarsten <l_karsten@gmx.de>
2024-04-21 23:37:31 +0200

    Es wird nun nicht mehr die Objektreferenz ausgegeben, sondern die Tatsächliche Karte jedes Spielers

--- src/test/java/hwr/oop/PlayerTest.java
+++ src/test/java/hwr/oop/PlayerTest.java
@@ -26,10 +26,10 @@ class PlayerTest {
         game.dealCards(deck.shuffleDeck());
 
       for (int i = 0; i < game.player1.ownCards.size(); i++) {
-          System.out.println("Spieler 1:" + game.player1.ownCards.get(i));
-          System.out.println("Spieler 2:" + game.player2.ownCards.get(i));
-          System.out.println("Spieler 3:" + game.player3.ownCards.get(i));
-          System.out.println("Spieler 4:" + game.player4.ownCards.get(i));
+          System.out.println("Spieler 1: " + game.player1.ownCards.get(i).color + " " + game.player1.ownCards.get(i).number);
+          System.out.println("Spieler 2: " + game.player2.ownCards.get(i).color + " " + game.player2.ownCards.get(i).number);
+          System.out.println("Spieler 3: " + game.player3.ownCards.get(i).color + " " + game.player3.ownCards.get(i).number);
+          System.out.println("Spieler 4: " + game.player4.ownCards.get(i).color + " " + game.player4.ownCards.get(i).number);
       }
 
         Assertions.assertThat(game.player1.ownCards.size()).isEqualTo(12);
