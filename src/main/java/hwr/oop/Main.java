c565d25c01960ffadf78716c3db44c5a1c6f100b
laura194 <voss.2000@gmx.de>
2024-04-21 12:50:36 +0200

    Main: Delete useless comments

--- src/main/java/hwr/oop/Main.java
+++ src/main/java/hwr/oop/Main.java
@@ -1,35 +1,8 @@
 package hwr.oop;
 import java.util.Arrays;
-import hwr.oop.exceptions.MovePieceException;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hi! Arguments: " + Arrays.asList(args));
-    /* ChessBoard board = new ChessBoard();
-      ChessBoard.printChessBoard(board.getBoard());
-      System.out.println("___________________________________");
-
-      try {
-        board.movePiece(new Position(6,0), new Position(4,0));
-      } catch (MovePieceException e) {
-        throw new RuntimeException(e);
-      }
-      ChessBoard.printChessBoard(board.getBoard());
-
-      System.out.println("___________________________________");
-      try {
-        board.movePiece(new Position(1,4), new Position(3,4));
-      } catch (MovePieceException e) {
-        throw new RuntimeException(e);
-      }
-      ChessBoard.printChessBoard(board.getBoard());
-
-      System.out.println("___________________________________");
-      try {
-        board.movePiece(new Position(7,0), new Position(5,0));
-      } catch (MovePieceException e) {
-        throw new RuntimeException(e);
-      }    ChessBoard.printChessBoard(board.getBoard());
-    }*/
   }
 }
