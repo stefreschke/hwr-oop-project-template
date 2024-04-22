921a0669356c65d3773a53fe139404d56488335e
Luroka <christophleptin@gmail.com>
2024-04-22 12:50:32 +0200

    Created playCardTest

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/GameEngineTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/GameEngineTest.java
@@ -1,39 +1,51 @@
 package hwr.oop.most_impressive_doppelkopf_experience;
 
+import hwr.oop.most_impressive_doppelkopf_experience.enums.CardColours;
+import hwr.oop.most_impressive_doppelkopf_experience.enums.CardSymbols;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 class GameEngineTest {
 
-    @Test
-    void handOutCardsTest() {
-        final var game = new Game();
-        final var players = game.handOutCards();
+  @Test
+  void handOutCardsTest() {
+    final var game = new Game();
+    final var players = game.handOutCards();
 
-        assertSoftly(softly->{
+    assertSoftly(
+        softly -> {
+          softly.assertThat(players.get(0).hand).isNotEmpty().isNotNull().hasSize(12);
 
-            softly.assertThat(players.get(0).hand)
-                    .isNotEmpty()
-                    .isNotNull()
-                    .hasSize(12);
+          softly.assertThat(players.get(1).hand).isNotEmpty().isNotNull().hasSize(12);
 
-            softly.assertThat(players.get(1).hand)
-                    .isNotEmpty()
-                    .isNotNull()
-                    .hasSize(12);
+          softly.assertThat(players.get(2).hand).isNotEmpty().isNotNull().hasSize(12);
 
-            softly.assertThat(players.get(2).hand)
-                    .isNotEmpty()
-                    .isNotNull()
-                    .hasSize(12);
-
-            softly.assertThat(players.get(3).hand)
-                    .isNotEmpty()
-                    .isNotNull()
-                    .hasSize(12);
+          softly.assertThat(players.get(3).hand).isNotEmpty().isNotNull().hasSize(12);
         });
+  }
 
-    }
+  @Test
+  void playCardTest() {
+    final var game = new Game();
+    final var player = new Player("player1", 0);
+    final var discardPile = new DiscardPile();
+
+    List<Card> hand = List.of(new Card(CardSymbols.NINE, CardColours.TRUMP, 10, "D9", 0));
+    player.setHand(hand);
+
+    int playerHandSize = player.getHand().size();
+    int discardPileSize = discardPile.discardCards.size();
+
+    game.playCard(player.getHand().get(0));
+
+    assertSoftly(
+            softly -> {
+              softly.assertThat(playerHandSize - 1).isEqualTo(player.getHand().size());
+
+              softly.assertThat(discardPileSize + 1).isEqualTo(discardPile.discardCards.size());
+            });
+  }
 }
