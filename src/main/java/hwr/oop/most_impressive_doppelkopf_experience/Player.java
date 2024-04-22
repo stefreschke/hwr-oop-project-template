2cca25138ee68a6809c9d0c2ce3ff1290b67a303
colin <colin.handro@gmail.com>
2024-04-22 12:53:37 +0200

    kein plan was hier passiert ist

--- src/main/java/hwr/oop/most_impressive_doppelkopf_experience/Player.java
+++ src/main/java/hwr/oop/most_impressive_doppelkopf_experience/Player.java
@@ -6,48 +6,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    String name;
+  String name;
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public int getScore() {
-        return score;
-    }
+  public int getScore() {
+    return score;
+  }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+  public void setScore(int score) {
+    this.score = score;
+  }
 
-    public TeamNames getTeam() {
-        return team;
-    }
+  public TeamNames getTeam() {
+    return team;
+  }
 
-    public void setTeam(TeamNames team) {
-        this.team = team;
-    }
+  public void setTeam(TeamNames team) {
+    this.team = team;
+  }
 
-    public List<Card> getHand() {
-        return hand;
-    }
+  public List<Card> getHand() {
+    return hand;
+  }
 
-    public void setHand(List<Card> hand) {
-        this.hand = hand;
-    }
+  public void setHand(List<Card> hand) {
+    this.hand = hand;
+  }
 
-    int score;
-    TeamNames team;
-    public List<Card> hand = new ArrayList<>();
+  int score;
+  TeamNames team;
+  public List<Card> hand = new ArrayList<>();
 
-    public Player(String name, int score) {
-        this.name = name;
-        this.score = score;
-    }
-
-
+  public Player(String name, int score) {
+    this.name = name;
+    this.score = score;
+  }
 }
