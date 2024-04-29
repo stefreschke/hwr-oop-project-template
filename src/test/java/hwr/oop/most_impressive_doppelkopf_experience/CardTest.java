ce1faabe3731ace2075d0f55138cc3b0b03f33e5
Srakawichi <nkmr9477@gmail.com>
2024-04-29 12:23:15 +0200

    CardTest completed

---
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CardTest.java
@@ -0,0 +1,50 @@
+package hwr.oop.most_impressive_doppelkopf_experience;
+
+import hwr.oop.most_impressive_doppelkopf_experience.enums.CardColours;
+import hwr.oop.most_impressive_doppelkopf_experience.enums.CardSymbols;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+public class CardTest {
+    @Test
+    void testCardSymbols() {
+        Card card = new Card(CardSymbols.TEN, CardColours.TRUMP, 12, "D10", 10);
+        assertSoftly(
+                softly -> {
+                    assertEquals(CardSymbols.TEN, card.getSymbol());
+                    assertEquals(CardColours.TRUMP, card.getColour());
+                    assertEquals(12, card.getValue());
+                    assertEquals("D10", card.getName());
+                    assertEquals(10, card.getWorth());
+                });
+    }
+
+    @Test
+    void testCardSymbols2() {
+        Card card = new Card(CardSymbols.JACK, CardColours.TRUMP, 14, "DJ", 2);
+        assertSoftly(
+                softly -> {
+                    assertEquals(CardSymbols.JACK, card.getSymbol());
+                    assertEquals(CardColours.TRUMP, card.getColour());
+                    assertEquals(14, card.getValue());
+                    assertEquals("DJ", card.getName());
+                    assertEquals(2, card.getWorth());
+                });
+    }
+
+    @Test
+    void testCardSymbols3() {
+        Card card = new Card(CardSymbols.ACE, CardColours.HEARTS, 4, "HA", 11);
+        assertSoftly(
+                softly -> {
+                    assertEquals(CardSymbols.ACE, card.getSymbol());
+                    assertEquals(CardColours.HEARTS, card.getColour());
+                    assertEquals(4, card.getValue());
+                    assertEquals("HA", card.getName());
+                    assertEquals(11, card.getWorth());
+                });
+    }
+
+}
