777fff5879fc94cc3ca5ef0bd9bcd1336142a838
Srakawichi <nkmr9477@gmail.com>
2024-05-06 10:50:39 +0200

    Card.java

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/DiscardPileTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/DiscardPileTest.java
@@ -5,6 +5,7 @@ import hwr.oop.most_impressive_doppelkopf_experience.enums.CardSymbols;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.Collections;
 import java.util.List;
@@ -38,5 +39,19 @@ public class DiscardPileTest {
     discardPile.findHighestValue();
 
     assertEquals(1, discardPile.getIdOfWinner());
+  }
+  @Test
+  void testgetDiscardPile() {
+    var discardPile = new DiscardPile();
+
+    var d9 = new Card(CardSymbols.NINE, CardColours.TRUMP, 10, "D9", 0);
+    var h10 = new Card(CardSymbols.TEN, CardColours.TRUMP, 100, "H10", 10);
+    var s9b = new Card(CardSymbols.NINE, CardColours.SPADES, 0, "S9", 0);
+    var cab = new Card(CardSymbols.ACE, CardColours.CLUBS, 4, "CA", 11);
+
+    discardPile.setDiscardCards(List.of(d9, h10, s9b, cab));
+    discardPile.getDiscardPile();
+
+    assertThat(discardPile.getDiscardPile()).hasSize(4);
   }
 }
