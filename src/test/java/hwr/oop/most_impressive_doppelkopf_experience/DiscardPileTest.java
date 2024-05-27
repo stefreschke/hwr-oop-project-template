f9680713d1300618ad4ac7dea294c03dcbafe935
colin <colin.handro@gmail.com>
2024-05-27 10:45:30 +0200

    getPositionOfHighestCardInDiscardPile running (Mutation still unfixed)

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/DiscardPileTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/DiscardPileTest.java
@@ -4,15 +4,12 @@ import hwr.oop.most_impressive_doppelkopf_experience.enums.CardColours;
 import hwr.oop.most_impressive_doppelkopf_experience.enums.CardSymbols;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import java.util.Collections;
 import java.util.List;
 
-public class DiscardPileTest {
+class DiscardPileTest {
   @Test
-  void findHighestValueTest() {
+  void getPositionOfHighestCardTest() {
+    var game = new Game();
     var discardPile = new DiscardPile();
 
     var d9 = new Card(CardSymbols.NINE, CardColours.TRUMP, 10, "D9", 0);
@@ -20,26 +17,13 @@ public class DiscardPileTest {
     var s9b = new Card(CardSymbols.NINE, CardColours.SPADES, 0, "S9", 0);
     var cab = new Card(CardSymbols.ACE, CardColours.CLUBS, 4, "CA", 5);
 
-    discardPile.setDiscardCards(List.of(d9, h10, s9b, cab));
+    discardPile.setDiscardCards(List.of(h10, d9 , s9b, cab));
 
-    var highestCard = discardPile.findHighestValue();
+    var highestCard = discardPile.getPositionOfHighestCardInDiscardPile();
 
-    assertThat(highestCard).isEqualTo(h10);
+    assertThat(highestCard).isEqualTo(0);
   }
-  @Test
-  void testgetIdOfWinner() {
-    var discardPile = new DiscardPile();
 
-    var d9 = new Card(CardSymbols.NINE, CardColours.TRUMP, 10, "D9", 0);
-    var h10 = new Card(CardSymbols.TEN, CardColours.TRUMP, 100, "H10", 10);
-    var s9b = new Card(CardSymbols.NINE, CardColours.SPADES, 0, "S9", 0);
-    var cab = new Card(CardSymbols.ACE, CardColours.CLUBS, 4, "CA", 5);
-
-    discardPile.setDiscardCards(List.of(d9, h10, s9b, cab));
-    var highestCard = discardPile.findHighestValue();
-
-    assertEquals(1, discardPile.getIdOfWinner());
-  }
   @Test
   void testgetDiscardPile() {
     var discardPile = new DiscardPile();
