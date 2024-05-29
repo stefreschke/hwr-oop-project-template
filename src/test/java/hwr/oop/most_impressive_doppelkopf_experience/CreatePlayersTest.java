595b2cc18e5af3832636b2e69bbd2e81d4470c15
Srakawichi <nkmr9477@gmail.com>
2024-05-29 09:43:59 +0200

    Update CreatePlayersTest.java

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CreatePlayersTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CreatePlayersTest.java
@@ -1,16 +1,11 @@
 package hwr.oop.most_impressive_doppelkopf_experience;
 import org.junit.jupiter.api.Test;
 import java.util.List;
+
+import static hwr.oop.most_impressive_doppelkopf_experience.Game.createPlayers;
 import static org.assertj.core.api.Assertions.assertThat;
 class CreatePlayersTest {
 
-    public List<Player> createPlayers() {
-        Player player1 = new Player("Colin", 0, 0);
-        Player player2 = new Player("Chrissi", 0, 1);
-        Player player3 = new Player("Mihoshi", 0, 2);
-        Player player4 = new Player("Josh", 0, 3);
-        return List.of(player1, player2, player3, player4);
-    }
     @Test
     void testCreatePlayers() {
         List<Player> players = createPlayers();
