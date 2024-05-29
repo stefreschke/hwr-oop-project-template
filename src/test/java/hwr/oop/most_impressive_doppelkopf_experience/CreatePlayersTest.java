c5d36f1dbe18fa445863666b51fcc1c5ec787d36
Srakawichi <nkmr9477@gmail.com>
2024-05-29 09:44:14 +0200

    Update CreatePlayersTest.java

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CreatePlayersTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CreatePlayersTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import static hwr.oop.most_impressive_doppelkopf_experience.Game.createPlayers;
 import static org.assertj.core.api.Assertions.assertThat;
 class CreatePlayersTest {
-
     @Test
     void testCreatePlayers() {
         List<Player> players = createPlayers();
