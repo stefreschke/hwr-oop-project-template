db2d4206a91522200ff9342d359f6ea9719b3579
Srakawichi <nkmr9477@gmail.com>
2024-06-05 12:35:09 +0200

    CreatePlayersTest.java coverage 100%

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CreatePlayersTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/CreatePlayersTest.java
@@ -1,7 +1,13 @@
 package hwr.oop.most_impressive_doppelkopf_experience;
+import hwr.oop.most_impressive_doppelkopf_experience.enums.CardColours;
+import hwr.oop.most_impressive_doppelkopf_experience.enums.CardSymbols;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CreatePlayersTest {
 
     public List<Player> createPlayers() {
@@ -15,4 +21,54 @@ class CreatePlayersTest {
     void testCreatePlayers() {
         List<Player> players = createPlayers();
         assertThat(players).hasSize(4);
-    }}
+    }
+    @Test
+    void setName() {
+        Player player = new Player("John", 0, 1);
+        player.setName("Jane");
+        assertEquals("Jane", player.getName());
+    }
+    @Test
+    void getPoints() {
+        Player player = new Player("John", 0, 1);
+        assertEquals(0, player.getPoints());
+    }
+    @Test
+    void setPoint() {
+        Player player = new Player("John", 0, 1);
+        player.setPoint(100);
+        assertEquals(100, player.getPoints());
+    }
+    @Test
+    void setScore() {
+        Player player = new Player("John", 0, 1);
+        player.setScore(50);
+        assertEquals(50, player.getScore());
+    }
+    @Test
+    void setCardsWon() {
+        Player player = new Player("John", 0, 1);
+        List<Card> cardsWon = new ArrayList<>();
+        cardsWon.add(new Card(CardSymbols.TEN, CardColours.TRUMP, 100, "H10", 10));
+        player.setCardsWon(cardsWon);
+        assertEquals(cardsWon, player.getCardsWon());
+    }
+    @Test
+    void calculateScore() {
+        Player player = new Player("John", 0, 1);
+        List<Card> cards = new ArrayList<>();
+        cards.add(new Card(CardSymbols.TEN, CardColours.TRUMP, 100, "H10", 10));
+        cards.add(new Card(CardSymbols.QUEEN, CardColours.TRUMP, 19, "HQ", 3));
+        assertEquals(13, player.calculateScore(cards));
+    }
+    @Test
+    void playerHasWonStich() {
+        Player player = new Player("John", 0, 1);
+        List<Card> stich = new ArrayList<>();
+        stich.add(new Card(CardSymbols.TEN, CardColours.TRUMP, 100, "H10", 10));
+        stich.add(new Card(CardSymbols.QUEEN, CardColours.TRUMP, 19, "HQ", 3));
+        player.playerHasWonStich(stich);
+        assertEquals(13, player.getScore());
+        assertEquals(stich, player.getCardsWon());
+    }
+}
