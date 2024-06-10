aa7f73ce024dc3123da58232cbb14f70b4b56acc
colin <colin.handro@gmail.com>
2024-06-10 16:11:00 +0200

    Test Fix PointsTest.java

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/PointsTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/PointsTest.java
@@ -124,7 +124,7 @@ class PointsTest {
     assertThat(game.calculateTeamScore(RE)).isEqualTo(120);
     assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(120);
     assertThat(game.calculateTeamPoints(RE)).isZero();
-    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(1);
+    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(2);
     assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
   }
 
@@ -147,7 +147,7 @@ class PointsTest {
     assertThat(game.calculateTeamScore(RE)).isZero();
     assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(240);
     assertThat(game.calculateTeamPoints(RE)).isZero();
-    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(5);
+    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(6);
     assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
   }
 
@@ -171,7 +171,7 @@ class PointsTest {
     assertThat(game.calculateTeamScore(RE)).isEqualTo(90);
     assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(150);
     assertThat(game.calculateTeamPoints(RE)).isZero();
-    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(1);
+    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(2);
     assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
   }
 
@@ -195,7 +195,7 @@ class PointsTest {
     assertThat(game.calculateTeamScore(RE)).isEqualTo(89);
     assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(151);
     assertThat(game.calculateTeamPoints(RE)).isZero();
-    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(2);
+    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(3);
     assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
   }
 
@@ -219,7 +219,7 @@ class PointsTest {
     assertThat(game.calculateTeamScore(RE)).isEqualTo(60);
     assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(180);
     assertThat(game.calculateTeamPoints(RE)).isZero();
-    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(2);
+    assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(3);
     assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
   }
   @Test
