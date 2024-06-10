0610005ee167e7ce14ec662220c37fae305c6788
joshnicot06 <j.tischer@fu-berlin.de>
2024-06-10 09:54:16 +0200

    Deleted Save game

--- src/main/java/hwr/oop/most_impressive_doppelkopf_experience/SaveGame.java
+++
@@ -1,9 +0,0 @@
-package hwr.oop.most_impressive_doppelkopf_experience;
-
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
-public class SaveGame {
-
-}
