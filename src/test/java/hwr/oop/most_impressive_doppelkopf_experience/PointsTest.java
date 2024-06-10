69b9180c309c93cf4b72f7252a17e16fcfdf4a02
colin <colin.handro@gmail.com>
2024-06-10 13:53:52 +0200

    next improvement on points test

--- src/test/java/hwr/oop/most_impressive_doppelkopf_experience/PointsTest.java
+++ src/test/java/hwr/oop/most_impressive_doppelkopf_experience/PointsTest.java
@@ -15,14 +15,14 @@ import static org.assertj.core.api.Assertions.assertThat;
         game.addPlayer("Galatea");
         game.addPlayer("Hajer");
 
-        game.players.getFirst().setScore(100);
-        game.players.getFirst().setTeam(RE);
-        game.players.get(1).setScore(20);
-        game.players.get(1).setTeam(RE);
-        game.players.get(2).setScore(30);
-        game.players.get(2).setTeam(CONTRA);
-        game.players.get(3).setScore(90);
-        game.players.get(3).setTeam(CONTRA);
+        game.getPlayers().getFirst().setScore(100);
+        game.getPlayers().getFirst().setTeam(RE);
+        game.getPlayers().get(1).setScore(20);
+        game.getPlayers().get(1).setTeam(RE);
+        game.getPlayers().get(2).setScore(30);
+        game.getPlayers().get(2).setTeam(CONTRA);
+        game.getPlayers().get(3).setScore(90);
+        game.getPlayers().get(3).setTeam(CONTRA);
 
         assertThat(game.calculateTeamScore(RE)).isEqualTo(120);
         assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(120);
@@ -30,10 +30,21 @@ import static org.assertj.core.api.Assertions.assertThat;
         assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(1);
         assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
 
-        game.players.getFirst().setScore(0);
-        game.players.get(1).setScore(0);
-        game.players.get(2).setScore(120);
-        game.players.get(3).setScore(120);
+        game.getPlayers().getFirst().setScore(121);
+        game.getPlayers().get(1).setScore(0);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(119);
+
+        assertThat(game.calculateTeamScore(RE)).isEqualTo(121);
+        assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(119);
+        assertThat(game.calculateTeamPoints(RE)).isEqualTo(1);
+        assertThat(game.calculateTeamPoints(CONTRA)).isZero();
+        assertThat(game.findWinningTeam()).isEqualTo(RE);
+
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(0);
+        game.getPlayers().get(2).setScore(120);
+        game.getPlayers().get(3).setScore(120);
 
         assertThat(game.calculateTeamScore(RE)).isZero();
         assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(240);
@@ -41,10 +52,22 @@ import static org.assertj.core.api.Assertions.assertThat;
         assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(5);
         assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
 
-        game.players.getFirst().setScore(0);
-        game.players.get(1).setScore(89);
-        game.players.get(2).setScore(0);
-        game.players.get(3).setScore(151);
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(90);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(150);
+
+        assertThat(game.calculateTeamScore(RE)).isEqualTo(90);
+        assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(150);
+        assertThat(game.calculateTeamPoints(RE)).isZero();
+        assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(1);
+        assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
+
+
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(89);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(151);
 
         assertThat(game.calculateTeamScore(RE)).isEqualTo(89);
         assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(151);
@@ -52,10 +75,21 @@ import static org.assertj.core.api.Assertions.assertThat;
         assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(2);
         assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
 
-        game.players.getFirst().setScore(0);
-        game.players.get(1).setScore(59);
-        game.players.get(2).setScore(0);
-        game.players.get(3).setScore(181);
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(60);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(180);
+
+        assertThat(game.calculateTeamScore(RE)).isEqualTo(60);
+        assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(180);
+        assertThat(game.calculateTeamPoints(RE)).isZero();
+        assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(2);
+        assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
+
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(59);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(181);
 
         assertThat(game.calculateTeamScore(RE)).isEqualTo(59);
         assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(181);
@@ -63,10 +97,22 @@ import static org.assertj.core.api.Assertions.assertThat;
         assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(3);
         assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
 
-        game.players.getFirst().setScore(0);
-        game.players.get(1).setScore(29);
-        game.players.get(2).setScore(0);
-        game.players.get(3).setScore(211);
+
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(30);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(210);
+
+        assertThat(game.calculateTeamScore(RE)).isEqualTo(30);
+        assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(210);
+        assertThat(game.calculateTeamPoints(RE)).isZero();
+        assertThat(game.calculateTeamPoints(CONTRA)).isEqualTo(3);
+        assertThat(game.findWinningTeam()).isEqualTo(CONTRA);
+
+        game.getPlayers().getFirst().setScore(0);
+        game.getPlayers().get(1).setScore(29);
+        game.getPlayers().get(2).setScore(0);
+        game.getPlayers().get(3).setScore(211);
 
         assertThat(game.calculateTeamScore(RE)).isEqualTo(29);
         assertThat(game.calculateTeamScore(CONTRA)).isEqualTo(211);
