eacffc237042ca0e4cd1f6b54f4cd09f4fecd1ba
colin <colin.handro@gmail.com>
2024-06-10 14:59:23 +0200

    Reworks on save and load game to fix bad mutation coverage

--- src/main/java/hwr/oop/most_impressive_doppelkopf_experience/GamePersistence.java
+++ src/main/java/hwr/oop/most_impressive_doppelkopf_experience/GamePersistence.java
@@ -1,25 +1,33 @@
 package hwr.oop.most_impressive_doppelkopf_experience;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.io.*;
 
 public class GamePersistence {
 
-    public void saveGame(Game game, String filePath) {
+    public void saveGame(Game game, String filePath) throws IOException {
         try (FileOutputStream fileOut = new FileOutputStream(filePath);
              ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
             out.writeObject(game);
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
     }
+    private static final Logger LOGGER = Logger.getLogger(GamePersistence.class.getName());
 
     public static Game loadGame(String filePath) {
         Game game = null;
         try (FileInputStream fileIn = new FileInputStream(filePath);
              ObjectInputStream in = new ObjectInputStream(fileIn)) {
             game = (Game) in.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e, () -> "IOException occurred while loading the game from " + filePath);
+        } catch (ClassNotFoundException e) {
+            LOGGER.log(Level.SEVERE, e, () -> "ClassNotFoundException occurred while loading the game from " + filePath);
         }
         return game;
     }
